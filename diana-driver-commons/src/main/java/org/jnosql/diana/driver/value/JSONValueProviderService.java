4438740d56ae5672ecbc61665dcb51810c037eeb
Otavio Santana <otaviojava@java.net>
2017-01-14 16:15:41 +0100

    Adds the header information

--- diana-driver-commons/src/main/java/org/jnosql/diana/driver/value/JSONValueProviderService.java
+++ diana-driver-commons/src/main/java/org/jnosql/diana/driver/value/JSONValueProviderService.java
@@ -20,24 +20,8 @@
 package org.jnosql.diana.driver.value;
 
 
-import org.jnosql.diana.api.Value;
+public final class JSONValueProviderService {
 
-import java.util.Objects;
-
-/**
- * The implementation that uses {@link JSONGSONValue}
- */
-public class JSONGSONValueProvider implements JSONValueProvider {
-
-    @Override
-    public Value of(String json) throws NullPointerException, UnsupportedOperationException {
-        Objects.requireNonNull(json, "Json is required");
-        return JSONGSONValue.of(json);
-    }
-
-    @Override
-    public Value of(byte[] json) throws NullPointerException, UnsupportedOperationException {
-        Objects.requireNonNull(json, "Json is required");
-        return JSONGSONValue.of(String.valueOf(json));
+    private JSONValueProviderService() {
     }
 }
