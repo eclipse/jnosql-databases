a295dad70661d778023289b630f1deeca7ebfe38
Otavio Santana <otaviojava@java.net>
2017-01-24 10:04:24 -0200

    Fixes checkstyle

--- couchbase-driver/src/test/java/org/jnosql/diana/couchbase/key/ProductCart.java
+++ couchbase-driver/src/test/java/org/jnosql/diana/couchbase/key/ProductCart.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jnosql.diana.couchbase.key;
 
 import java.io.Serializable;
@@ -12,6 +30,12 @@ public class ProductCart implements Serializable {
 
     private final BigDecimal price;
 
+
+    public ProductCart(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
@@ -21,10 +45,6 @@ public class ProductCart implements Serializable {
     }
 
 
-    public ProductCart(String name, BigDecimal price) {
-        this.name = name;
-        this.price = price;
-    }
 
     @Override
     public boolean equals(Object o) {
