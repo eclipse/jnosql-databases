b90af5184c5d1df4ecf94e7291ad4bb39c474a7c
Otavio Santana <otaviojava@java.net>
2017-01-27 17:03:43 -0200

    Creates modules on elasticsearch

---
+++ elasticsearch-driver/src/main/java/org/jnosql/diana/elasticsearch/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ *  <p>Elasticsearch is a search engine based on Lucene. It provides a distributed, multitenant-capable full-text search
+ *  engine with an HTTP web interface and schema-free JSON documents. Elasticsearch is developed in Java and is released
+ *  as open source under the terms of the Apache License. Elasticsearch is the most popular enterprise search engine
+ *  followed by Apache Solr, also based on Lucene.</p>
+ *  <p>It is developed alongside a data collection and log parsing engine called Logstash,
+ *  and an analytics and visualization platform called Kibana. The three products are designed to be used as an integrated
+ *  solution, referred to as the "ELK stack".</p>
+ */
+package org.jnosql.diana.elasticsearch;
