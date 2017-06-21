86c20cf182378f684720fc09250217864fc2d61a
Otavio Santana <otaviopolianasantana@gmail.com>
2017-06-21 18:32:39 -0300

    fix Queue on couchbase

--- couchbase-driver/src/main/java/org/jnosql/diana/couchbase/key/CouchbaseQueue.java
+++ couchbase-driver/src/main/java/org/jnosql/diana/couchbase/key/CouchbaseQueue.java
@@ -15,13 +15,14 @@
 package org.jnosql.diana.couchbase.key;
 
 import com.couchbase.client.java.Bucket;
-import org.jnosql.diana.driver.value.JSONValueProvider;
-import org.jnosql.diana.driver.value.JSONValueProviderService;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.Queue;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -31,13 +32,13 @@ import static java.util.Objects.requireNonNull;
  * The couchbase implementation to {@link Queue}
  * that avoid null items, so if any null object will launch {@link NullPointerException}.
  * This class is a wrapper to {@link com.couchbase.client.java.datastructures.collections.CouchbaseQueue}. Once they only can save primitive type,
- * objects are converted to Json {@link String} using {@link JSONValueProvider#toJson(Object)}
+ * objects are converted to Json {@link String} using {@link Jsonb#toJson(Object)}
  *
  * @param <T> the object to be stored.
  */
 public class CouchbaseQueue<T> implements Queue<T> {
 
-    private static final JSONValueProvider PROVDER = JSONValueProviderService.getProvider();
+    private static final Jsonb JSONB = JsonbBuilder.create();
 
 
     private final String bucketName;
@@ -81,7 +82,7 @@ public class CouchbaseQueue<T> implements Queue<T> {
     @Override
     public boolean offer(T t) {
         requireNonNull(t, "object is required");
-        return queue.offer(PROVDER.toJson(t));
+        return queue.offer(JSONB.toJson(t));
     }
 
     @Override
@@ -111,7 +112,7 @@ public class CouchbaseQueue<T> implements Queue<T> {
 
     private T getT(String json) {
         if (Objects.nonNull(json)) {
-            return PROVDER.of(json).get(clazz);
+            return JSONB.fromJson(json, clazz);
         }
         return null;
     }
@@ -119,32 +120,32 @@ public class CouchbaseQueue<T> implements Queue<T> {
     @Override
     public boolean removeAll(Collection<?> collection) {
         requireNonNull(collection, "collection is required");
-        return queue.removeAll(collection.stream().map(PROVDER::toJson).collect(Collectors.toList()));
+        return queue.removeAll(collection.stream().map(JSONB::toJson).collect(Collectors.toList()));
     }
 
     @Override
     public boolean remove(Object o) {
         Objects.requireNonNull(o, "object is required");
-        return queue.remove(PROVDER.toJson(o));
+        return queue.remove(JSONB.toJson(o));
     }
 
     @Override
     public boolean contains(Object o) {
         Objects.requireNonNull(o, "object is required");
-        return queue.contains(PROVDER.toJson(o));
+        return queue.contains(JSONB.toJson(o));
     }
 
     @Override
     public Iterator<T> iterator() {
         return StreamSupport.stream(queue.spliterator(), false)
-                .map(s -> PROVDER.of(s).get(clazz))
+                .map(fromJSON())
                 .collect(Collectors.toList()).iterator();
     }
 
     @Override
     public Object[] toArray() {
         return StreamSupport.stream(queue.spliterator(), false)
-                .map(s -> PROVDER.of(s).get(clazz))
+                .map(fromJSON())
                 .toArray(size -> new Object[size]);
     }
 
@@ -152,20 +153,25 @@ public class CouchbaseQueue<T> implements Queue<T> {
     public <T1> T1[] toArray(T1[] t1s) {
         requireNonNull(t1s, "arrys is required");
         return StreamSupport.stream(queue.spliterator(), false)
-                .map(s -> PROVDER.of(s).get(clazz))
+                .map(fromJSON())
                 .toArray(size -> t1s);
     }
 
     @Override
     public boolean retainAll(Collection<?> collection) {
         requireNonNull(collection, "collection is required");
-        return queue.retainAll(collection.stream().map(PROVDER::toJson).collect(Collectors.toList()));
+        return queue.retainAll(collection.stream().map(JSONB::toJson).collect(Collectors.toList()));
     }
 
     @Override
     public boolean containsAll(Collection<?> collection) {
         requireNonNull(collection, "collection is required");
-        return queue.containsAll(collection.stream().map(PROVDER::toJson).collect(Collectors.toList()));
+        return queue.containsAll(collection.stream().map(JSONB::toJson).collect(Collectors.toList()));
     }
+
+    private Function<String, T> fromJSON() {
+        return s -> JSONB.fromJson(s, clazz);
+    }
+
 
 }
