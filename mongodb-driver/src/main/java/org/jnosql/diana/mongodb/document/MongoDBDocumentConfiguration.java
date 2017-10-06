3b6ba3cbf6bb1e007b5d6d6e84f1e1d765bafac2
Otavio Santana <otaviopolianasantana@gmail.com>
2017-10-06 10:29:43 -0700

    Fixes javadoc

--- mongodb-driver/src/main/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfiguration.java
+++ mongodb-driver/src/main/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfiguration.java
@@ -35,7 +35,8 @@ import static java.util.Objects.requireNonNull;
 
 
 /**
- * The MongoDB implementation to {@link UnaryDocumentConfiguration} that returns  {@link MongoDBDocumentCollectionManagerFactory}.
+ * The MongoDB implementation to both {@link DocumentConfiguration} and {@link DocumentConfigurationAsync}
+ * that returns  {@link MongoDBDocumentCollectionManagerFactory} {@link MongoDBDocumentCollectionManagerAsyncFactory}.
  * It tries to read the diana-mongodb.properties file whose has the following properties
  * <p>mongodb-server-host-: as prefix to add host client, eg: mongodb-server-host-1=host1, mongodb-server-host-2= host2</p>
  */
