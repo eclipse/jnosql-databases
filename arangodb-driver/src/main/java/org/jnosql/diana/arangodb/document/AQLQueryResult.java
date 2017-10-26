b042bc6fccb5154c84e41a8bff525f7d7cfa6166
Otavio Santana <otaviopolianasantana@gmail.com>
2017-10-26 18:20:43 -0200

    Creates AQL query

---
+++ arangodb-driver/src/main/java/org/jnosql/diana/arangodb/document/AQLQueryResult.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.diana.arangodb.document;
+
+import java.util.Map;
+
+class AQLQueryResult {
+    private final String query;
+
+    private final Map<String, Object> values;
+
+    public AQLQueryResult(String query, Map<String, Object> values) {
+        this.query = query;
+        this.values = values;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public Map<String, Object> getValues() {
+        return values;
+    }
+}
