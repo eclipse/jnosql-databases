54df1c99c2fb0311a8931dd7340f2222150564f3
Otavio Santana <otaviopolianasantana@gmail.com>
2017-10-26 22:55:23 -0200

    Upgrades to async

--- arangodb-driver/src/main/java/org/jnosql/diana/arangodb/document/ArangoDBDocumentCollectionManagerAsync.java
+++ arangodb-driver/src/main/java/org/jnosql/diana/arangodb/document/ArangoDBDocumentCollectionManagerAsync.java
@@ -36,6 +36,7 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
@@ -51,161 +52,22 @@ import static org.jnosql.diana.arangodb.document.ArangoDBUtil.toEntity;
  * <p>{@link DocumentCollectionManagerAsync#insert(DocumentEntity, Duration)}</p>
  * <p>{@link DocumentCollectionManagerAsync#insert(DocumentEntity, Duration, Consumer)}</p>
  */
-public class ArangoDBDocumentCollectionManagerAsync implements DocumentCollectionManagerAsync {
+public interface ArangoDBDocumentCollectionManagerAsync extends DocumentCollectionManagerAsync {
 
-
-    private final ArangoDB arangoDB;
-
-    private final ArangoDBAsync arangoDBAsync;
-
-    private final String database;
-
-    ArangoDBDocumentCollectionManagerAsync(String database, ArangoDB arangoDB, ArangoDBAsync arangoDBAsync) {
-        this.arangoDB = arangoDB;
-        this.arangoDBAsync = arangoDBAsync;
-        this.database = database;
-    }
-
-    @Override
-    public void insert(DocumentEntity entity) throws ExecuteAsyncQueryException, UnsupportedOperationException {
-        insert(entity, v -> {
-        });
-    }
-
-    @Override
-    public void insert(DocumentEntity entity, Duration ttl) throws ExecuteAsyncQueryException, UnsupportedOperationException {
-        throw new UnsupportedOperationException("TTL is not supported on ArangoDB implementation");
-    }
-
-
-    @Override
-    public void insert(DocumentEntity entity, Consumer<DocumentEntity> callBack) throws ExecuteAsyncQueryException
-            , UnsupportedOperationException {
-
-        String collectionName = entity.getName();
-        checkCollection(collectionName);
-        BaseDocument baseDocument = getBaseDocument(entity);
-        CompletableFuture<DocumentCreateEntity<BaseDocument>> future = arangoDBAsync.db(database)
-                .collection(collectionName).insertDocument(baseDocument);
-        future.thenAccept(d -> callBack.accept(entity));
-    }
-
-    @Override
-    public void insert(DocumentEntity entity, Duration ttl, Consumer<DocumentEntity> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException {
-        throw new UnsupportedOperationException("TTL is not supported on ArangoDB implementation");
-    }
-
-    @Override
-    public void update(DocumentEntity entity) throws ExecuteAsyncQueryException, UnsupportedOperationException {
-        update(entity, v -> {
-        });
-    }
-
-    @Override
-    public void update(DocumentEntity entity, Consumer<DocumentEntity> callBack)
-            throws ExecuteAsyncQueryException, UnsupportedOperationException {
-        String collectionName = entity.getName();
-        checkCollection(collectionName);
-        BaseDocument baseDocument = getBaseDocument(entity);
-        CompletableFuture<DocumentUpdateEntity<BaseDocument>> future = arangoDBAsync.db(database).collection(collectionName)
-                .updateDocument(baseDocument.getKey(), baseDocument);
-        future.thenAccept(d -> callBack.accept(entity));
-    }
-
-
-    @Override
-    public void delete(DocumentDeleteQuery query) throws ExecuteAsyncQueryException, UnsupportedOperationException {
-        delete(query, v -> {
-        });
-    }
-
-    @Override
-    public void delete(DocumentDeleteQuery query, Consumer<Void> callBack)
-            throws ExecuteAsyncQueryException, UnsupportedOperationException {
-
-        Objects.requireNonNull(query, "query is required");
-        Objects.requireNonNull(callBack, "callBack is required");
-
-        String collection = query.getDocumentCollection();
-        if (checkCondition(query.getCondition())) {
-            return;
-        }
-        DocumentCondition condition = query.getCondition()
-                .orElseThrow(() -> new IllegalArgumentException("Condition is required"));
-        Value value = condition.getDocument().getValue();
-        if (Condition.IN.equals(condition.getCondition())) {
-            List<String> keys = value.get(new TypeReference<List<String>>() {
-            });
-            CompletableFuture<MultiDocumentEntity<DocumentDeleteEntity<Void>>> future = arangoDBAsync.db(database)
-                    .collection(collection).deleteDocuments(keys);
-            future.thenAccept(d -> callBack.accept(null));
-        } else if (Condition.IN.equals(condition.getCondition())) {
-            String key = value.get(String.class);
-            CompletableFuture<DocumentDeleteEntity<Void>> future = arangoDBAsync.db(database).
-                    collection(collection).deleteDocument(key);
-            future.thenAccept(d -> callBack.accept(null));
-        }
-
-    }
-
-    @Override
-    public void select(DocumentQuery query, Consumer<List<DocumentEntity>> callBack)
-            throws ExecuteAsyncQueryException, UnsupportedOperationException {
-
-        Objects.requireNonNull(query, "query is required");
-        Objects.requireNonNull(callBack, "callBack is required");
-        if (checkCondition(query.getCondition())) {
-            callBack.accept(Collections.emptyList());
-            return;
-        }
-        DocumentCondition condition = query.getCondition()
-                .orElseThrow(() -> new IllegalArgumentException("Condition is required"));
-        Value value = condition.getDocument().getValue();
-        String collection = query.getDocumentCollection();
-        if (Condition.EQUALS.equals(condition.getCondition())) {
-            String key = value.get(String.class);
-            CompletableFuture<BaseDocument> future = arangoDBAsync.db(database).collection(collection)
-                    .getDocument(key, BaseDocument.class);
-
-            future.thenAccept(d -> callBack.accept(singletonList(toEntity(d))));
-
-            return;
-        }
-        if (Condition.IN.equals(condition.getCondition())) {
-            List<String> keys = value.get(new TypeReference<List<String>>() {
-            });
-            List<DocumentEntity> entities = synchronizedList(new ArrayList<>());
-
-            if (keys.isEmpty()) {
-                callBack.accept(Collections.emptyList());
-                return;
-            }
-
-            List<CompletableFuture<Void>> futures = new ArrayList<>();
-            for (String key : keys) {
-                CompletableFuture<BaseDocument> future = arangoDBAsync.db(database).collection(collection)
-                        .getDocument(key, BaseDocument.class);
-                futures.add(future.thenAcceptAsync(d -> entities.add(toEntity(d))));
-            }
-            CompletableFuture.allOf(futures.toArray(
-                    new CompletableFuture[futures.size()]))
-                    .thenAcceptAsync(v -> callBack.accept(entities));
-
-        }
-
-        callBack.accept(Collections.emptyList());
-
-    }
-
-
-    @Override
-    public void close() {
-
-    }
-
-    private void checkCollection(String collectionName) {
-        ArangoDBUtil.checkCollection(database, arangoDB, collectionName);
-    }
+    /**
+     * Executes AQL, finds {@link DocumentEntity} from select asynchronously
+     * <p>FOR u IN users FILTER u.status == @status RETURN u </p>
+     *
+     * @param query    the query
+     * @param values   the named queries
+     * @param callBack the callback, when the process is finished will call this instance returning
+     *                 the result of select within parameters
+     * @throws ExecuteAsyncQueryException    when there is a async error
+     * @throws UnsupportedOperationException when the database does not support this feature
+     * @throws NullPointerException          when either select or callback are null
+     */
+    void aql(String query, Map<String, Object> values, Consumer<List<DocumentEntity>> callBack) throws
+            ExecuteAsyncQueryException, UnsupportedOperationException, NullPointerException;
 
 
 }
