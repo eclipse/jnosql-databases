035c809ea76f9735cd4fc4b219ba736ef974b201
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-23 17:34:23 -0200

    adds constant

--- cassandra-driver/src/test/java/org/jnosql/diana/cassandra/column/Constants.java
+++ cassandra-driver/src/test/java/org/jnosql/diana/cassandra/column/Constants.java
@@ -19,6 +19,6 @@ package org.jnosql.diana.cassandra.column;
 public final class Constants {
 
     public static final String KEY_SPACE = "newKeySpace";
-    public static final String COLUMN_FAMILY = "newColumnFamily";
+    public static final String COLUMN_FAMILY = "newcolumnfamily";
     private Constants() {}
 }
