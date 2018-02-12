d43d25bfad2d66bbcd204123ab88dd28496602b3
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-12 08:27:47 -0200

    removes check by key verification

--- arangodb-driver/src/main/java/org/jnosql/diana/arangodb/document/OperationsByKeysUtils.java
+++ arangodb-driver/src/main/java/org/jnosql/diana/arangodb/document/OperationsByKeysUtils.java
@@ -19,25 +19,17 @@ import com.arangodb.ArangoDBAsync;
 import com.arangodb.entity.BaseDocument;
 import com.arangodb.entity.DocumentDeleteEntity;
 import com.arangodb.entity.MultiDocumentEntity;
-import org.jnosql.diana.api.Condition;
 import org.jnosql.diana.api.TypeReference;
 import org.jnosql.diana.api.Value;
 import org.jnosql.diana.api.document.DocumentCondition;
 import org.jnosql.diana.api.document.DocumentDeleteQuery;
 import org.jnosql.diana.api.document.DocumentEntity;
-import org.jnosql.diana.api.document.DocumentQuery;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
-import static java.util.Collections.singletonList;
-import static java.util.Collections.synchronizedList;
-import static java.util.stream.Collectors.toList;
 import static org.jnosql.diana.api.Condition.EQUALS;
 import static org.jnosql.diana.api.Condition.IN;
 
@@ -46,67 +38,7 @@ final class OperationsByKeysUtils {
     private OperationsByKeysUtils() {
     }
 
-    public static List<DocumentEntity> findByKeys(DocumentQuery query, ArangoDB arangoDB, String database) {
-        DocumentCondition condition = query.getCondition().get();
-        Value value = condition.getDocument().getValue();
-        String collection = query.getDocumentCollection();
-        if (EQUALS.equals(condition.getCondition())) {
-            String key = value.get(String.class);
-            DocumentEntity entity = toEntity(collection, key, arangoDB, database);
-            if (Objects.isNull(entity)) {
-                return Collections.emptyList();
-            }
-            return singletonList(entity);
-        } else if (IN.equals(condition.getCondition())) {
-            List<String> keys = value.get(new TypeReference<List<String>>() {
-            });
-            return keys.stream().map(k -> toEntity(collection, k, arangoDB, database))
-                    .collect(toList());
-        }
 
-        return Collections.emptyList();
-    }
-
-    public static void findByKeys(DocumentQuery query, Consumer<List<DocumentEntity>> callBack,
-                                  ArangoDBAsync arangoDBAsync, String database) {
-
-        DocumentCondition condition = query.getCondition()
-                .orElseThrow(() -> new IllegalArgumentException("Condition is required"));
-        Value value = condition.getDocument().getValue();
-        String collection = query.getDocumentCollection();
-        if (Condition.EQUALS.equals(condition.getCondition())) {
-            String key = value.get(String.class);
-            CompletableFuture<BaseDocument> future = arangoDBAsync.db(database).collection(collection)
-                    .getDocument(key, BaseDocument.class);
-
-            future.thenAccept(d -> callBack.accept(singletonList(ArangoDBUtil.toEntity(d))));
-
-            return;
-        }
-        if (IN.equals(condition.getCondition())) {
-            List<String> keys = value.get(new TypeReference<List<String>>() {
-            });
-            List<DocumentEntity> entities = synchronizedList(new ArrayList<>());
-
-            if (keys.isEmpty()) {
-                callBack.accept(Collections.emptyList());
-                return;
-            }
-
-            List<CompletableFuture<Void>> futures = new ArrayList<>();
-            for (String key : keys) {
-                CompletableFuture<BaseDocument> future = arangoDBAsync.db(database).collection(collection)
-                        .getDocument(key, BaseDocument.class);
-                futures.add(future.thenAcceptAsync(d -> entities.add(ArangoDBUtil.toEntity(d))));
-            }
-            CompletableFuture.allOf(futures.toArray(
-                    new CompletableFuture[futures.size()]))
-                    .thenAcceptAsync(v -> callBack.accept(entities));
-
-        }
-
-        callBack.accept(Collections.emptyList());
-    }
 
     private static DocumentEntity toEntity(String collection, String key, ArangoDB arangoDB, String database) {
         BaseDocument document = arangoDB.db(database).collection(collection).getDocument(key, BaseDocument.class);
@@ -116,16 +48,6 @@ final class OperationsByKeysUtils {
         return ArangoDBUtil.toEntity(document);
     }
 
-    public static boolean isJustKey(Optional<DocumentCondition> documentCondition, String key) {
-        if (documentCondition.isPresent()) {
-            DocumentCondition condition = documentCondition.get();
-            boolean isKeyDocument = key.equals(condition.getDocument().getName());
-            boolean isEqualsKeys = EQUALS.equals(condition.getCondition()) && isKeyDocument;
-            boolean isINKeys = IN.equals(condition.getCondition()) && isKeyDocument;
-            return isEqualsKeys || isINKeys;
-        }
-        return false;
-    }
 
     public static void deleteByKey(DocumentDeleteQuery query, String collection, ArangoDB arangoDB, String database) {
         DocumentCondition condition = query.getCondition()
