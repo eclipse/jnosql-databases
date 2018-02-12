0c0e03020f298c2a71020186e3752aee939ed3e1
Lucas Furlaneto <lucasfurlaneto.s@gmail.com>
2018-02-12 10:07:23 -0200

    improvements tests on update
    Signed-off-by: Lucas Furlaneto <lucasfurlaneto.s@gmail.com>

--- orientdb-driver/src/test/java/org/jnosql/diana/orientdb/document/OrientDBDocumentCollectionManagerTest.java
+++ orientdb-driver/src/test/java/org/jnosql/diana/orientdb/document/OrientDBDocumentCollectionManagerTest.java
@@ -82,12 +82,19 @@ public class OrientDBDocumentCollectionManagerTest {
 
     @Test
     public void shouldUpdateSave() {
-        DocumentEntity entity = getEntity();
-        DocumentEntity documentEntity = entityManager.insert(entity);
+        DocumentEntity entity = entityManager.insert(getEntity());
         Document newField = Documents.of("newField", "10");
         entity.add(newField);
-        DocumentEntity updated = entityManager.update(entity);
-        assertEquals(newField, updated.find("newField").get());
+        entityManager.update(entity);
+
+        Document id = entity.find(OrientDBConverter.RID_FIELD).get();
+        DocumentQuery query = select().from(entity.getName())
+                .where(id.getName()).eq(id.get())
+                .build();
+        Optional<DocumentEntity> updated = entityManager.singleResult(query);
+
+        assertTrue(updated.isPresent());
+        assertEquals(newField, updated.get().find("newField").get());
     }
 
     @Test
