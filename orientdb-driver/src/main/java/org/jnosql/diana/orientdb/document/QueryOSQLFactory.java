84d35850f1896f27552ce0ef045f2da4e4ca8eb5
Lucas Furlaneto <lucasfurlaneto.s@gmail.com>
2018-02-13 12:53:06 -0200

    Create query factory and converter

--- orientdb-driver/src/main/java/org/jnosql/diana/orientdb/document/QueryOSQLFactory.java
+++ orientdb-driver/src/main/java/org/jnosql/diana/orientdb/document/QueryOSQLFactory.java
@@ -11,9 +11,9 @@
  *   Contributors:
  *
  *   Otavio Santana
+ *   Lucas Furlaneto
  */
 package org.jnosql.diana.orientdb.document;
-
 
 import com.orientechnologies.orient.core.command.OCommandResultListener;
 import com.orientechnologies.orient.core.record.impl.ODocument;
@@ -21,12 +21,9 @@ import com.orientechnologies.orient.core.sql.query.OLiveQuery;
 import com.orientechnologies.orient.core.sql.query.OSQLAsynchQuery;
 import com.orientechnologies.orient.core.sql.query.OSQLQuery;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
-import org.jnosql.diana.api.Condition;
-import org.jnosql.diana.api.TypeReference;
-import org.jnosql.diana.api.document.Document;
-import org.jnosql.diana.api.document.DocumentCondition;
 import org.jnosql.diana.api.document.DocumentEntity;
 import org.jnosql.diana.api.document.DocumentQuery;
+import org.jnosql.diana.orientdb.document.QueryOSQLConverter.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,15 +31,15 @@ import java.util.function.Consumer;
 
 import static java.util.Arrays.asList;
 
-final class OSQLQueryFactory {
+final class QueryOSQLFactory {
 
     public static final String LIVE = "live ";
 
-    private OSQLQueryFactory() {
+    private QueryOSQLFactory() {
     }
 
     static QueryResult to(DocumentQuery documentQuery) {
-        Query query = getQuery(documentQuery);
+        Query query = QueryOSQLConverter.select(documentQuery);
 
         return new QueryResult(new OSQLSynchQuery<ODocument>(query.getQuery()) {
         }, query.getParams());
@@ -54,7 +51,7 @@ final class OSQLQueryFactory {
     }
 
     static QueryResult toAsync(DocumentQuery documentQuery, Consumer<List<ODocument>> callBack) {
-        Query query = getQuery(documentQuery);
+        Query query = QueryOSQLConverter.select(documentQuery);
         return new QueryResult(new OSQLAsynchQuery<>(query.getQuery(), new OCommandResultListener() {
             private List<ODocument> documents = new ArrayList<>();
 
@@ -78,7 +75,7 @@ final class OSQLQueryFactory {
     }
 
     static QueryResult toLive(DocumentQuery documentQuery, Consumer<DocumentEntity> callBack) {
-        Query query = getQuery(documentQuery);
+        Query query = QueryOSQLConverter.select(documentQuery);
         OLiveQuery<ODocument> liveQuery = new OLiveQuery<>(LIVE + query.getQuery(), new LiveQueryLIstener(callBack));
         return new QueryResult(liveQuery, query.getParams());
 
@@ -105,96 +102,6 @@ final class OSQLQueryFactory {
                 return null;
             }
         }), asList(params));
-    }
-
-
-    private static Query getQuery(DocumentQuery documentQuery) {
-        StringBuilder query = new StringBuilder();
-        List<Object> params = new java.util.ArrayList<>();
-        query.append("SELECT FROM ");
-        query.append(documentQuery.getDocumentCollection());
-        int counter = 0;
-        if (documentQuery.getCondition().isPresent()) {
-            query.append(" WHERE ");
-            counter = addCondition(documentQuery.getCondition().get(), counter, query, params);
-        }
-        return new Query(query.toString(), params);
-    }
-
-    private static int addCondition(DocumentCondition documentCondition, int counter, StringBuilder query, List<Object> params) {
-        return addCondition(documentCondition, counter, query, params, "AND");
-    }
-
-    private static int addCondition(DocumentCondition documentCondition, int counter, StringBuilder query, List<Object> params, String connector) {
-        Document document = documentCondition.getDocument();
-        Condition condition = documentCondition.getCondition();
-        int aux = counter;
-        if (Condition.AND.equals(condition) || Condition.OR.equals(condition) || Condition.NOT.equals(condition)) {
-            List<DocumentCondition> documentConditions = document.get(new TypeReference<List<DocumentCondition>>() {
-            });
-
-            for (DocumentCondition dc : documentConditions) {
-                aux = addCondition(dc, aux, query, params, toCondition(condition));
-            }
-            return aux;
-
-        }
-        if (counter > 0) {
-            query.append(' ').append(connector).append(' ');
-        }
-        query.append(document.getName())
-                .append(' ')
-                .append(toCondition(condition))
-                .append(' ')
-                .append('?');
-        params.add(document.get());
-        return aux + 1;
-    }
-
-    private static String toCondition(Condition condition) {
-        switch (condition) {
-            case AND:
-                return "AND";
-            case EQUALS:
-                return "=";
-            case GREATER_EQUALS_THAN:
-                return ">=";
-            case GREATER_THAN:
-                return ">";
-            case IN:
-                return "IN";
-            case LESSER_EQUALS_THAN:
-                return "<=";
-            case LESSER_THAN:
-                return "<";
-            case LIKE:
-                return "LIKE";
-            case NOT:
-                return "NOT";
-            case OR:
-                return "OR";
-            default:
-                throw new IllegalArgumentException("Orient DB has not support to the condition " + condition);
-
-        }
-    }
-
-    static class Query {
-        private final String query;
-        private final List<Object> params;
-
-        Query(String query, List<Object> params) {
-            this.query = query;
-            this.params = params;
-        }
-
-        String getQuery() {
-            return query;
-        }
-
-        List<Object> getParams() {
-            return params;
-        }
     }
 
     static class QueryResult {
