050f335fed47ff90a49ef67fe6f1181f0e4ab0e0
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-13 06:33:20 -0200

    adds constants

--- arangodb-driver/src/main/java/org/jnosql/diana/arangodb/document/AQLUtils.java
+++ arangodb-driver/src/main/java/org/jnosql/diana/arangodb/document/AQLUtils.java
@@ -30,6 +30,9 @@ import java.util.Optional;
 final class AQLUtils {
 
     private static final String FILTER = " FILTER ";
+    private static final String LIMIT = " LIMIT ";
+    private static final String IN = " IN ";
+    private static final String SORT = " SORT ";
 
     private AQLUtils() {
     }
@@ -65,7 +68,7 @@ final class AQLUtils {
         StringBuilder aql = new StringBuilder();
         Map<String, Object> params = new HashMap<>();
         char entity = Character.toLowerCase(documentCollection.charAt(0));
-        aql.append("FOR ").append(entity).append(" IN ").append(documentCollection);
+        aql.append("FOR ").append(entity).append(IN).append(documentCollection);
 
         documentCondition.ifPresent(condition -> {
             aql.append(FILTER);
@@ -76,21 +79,21 @@ final class AQLUtils {
         }
 
         if (firstResult > 0 && maxResult > 0) {
-            aql.append(" LIMIT ").append(firstResult)
+            aql.append(LIMIT).append(firstResult)
                     .append(", ").append(maxResult);
         } else if (maxResult > 0) {
-            aql.append(" LIMIT ").append(maxResult);
+            aql.append(LIMIT).append(maxResult);
         }
 
         aql.append(conclusion).append(entity);
         if (delete) {
-            aql.append(" IN ").append(documentCollection);
+            aql.append(IN).append(documentCollection);
         }
         return new AQLQueryResult(aql.toString(), params);
     }
 
     private static void sort(List<Sort> sorts, StringBuilder aql, char entity) {
-        aql.append(" SORT ");
+        aql.append(SORT);
         String separator = " ";
         for (Sort sort : sorts) {
             aql.append(separator)
@@ -104,56 +107,56 @@ final class AQLUtils {
     private static void definesCondition(DocumentCondition condition,
                                          StringBuilder aql,
                                          Map<String, Object> params,
-                                         char entity, int count) {
+                                         char entity, int counter) {
 
         Document document = condition.getDocument();
         switch (condition.getCondition()) {
             case IN:
-                appendCondtion(aql, params, entity, document, " IN ");
+                appendCondition(aql, params, entity, document, IN);
                 return;
             case EQUALS:
-                appendCondtion(aql, params, entity, document, " == ");
+                appendCondition(aql, params, entity, document, " == ");
                 return;
             case GREATER_EQUALS_THAN:
-                appendCondtion(aql, params, entity, document, " >= ");
+                appendCondition(aql, params, entity, document, " >= ");
                 return;
             case GREATER_THAN:
-                appendCondtion(aql, params, entity, document, " > ");
+                appendCondition(aql, params, entity, document, " > ");
                 return;
             case LESSER_THAN:
-                appendCondtion(aql, params, entity, document, " < ");
+                appendCondition(aql, params, entity, document, " < ");
                 return;
             case LESSER_EQUALS_THAN:
-                appendCondtion(aql, params, entity, document, " <= ");
+                appendCondition(aql, params, entity, document, " <= ");
                 return;
             case LIKE:
-                appendCondtion(aql, params, entity, document, " LIKE ");
+                appendCondition(aql, params, entity, document, " LIKE ");
                 return;
             case AND:
 
                 for (DocumentCondition dc : document.get(new TypeReference<List<DocumentCondition>>() {
                 })) {
 
-                    if (isFirstCondition(aql, count)) {
+                    if (isFirstCondition(aql, counter)) {
                         aql.append(" AND ");
                     }
-                    definesCondition(dc, aql, params, entity, ++count);
+                    definesCondition(dc, aql, params, entity, ++counter);
                 }
                 return;
             case OR:
 
                 for (DocumentCondition dc : document.get(new TypeReference<List<DocumentCondition>>() {
                 })) {
-                    if (isFirstCondition(aql, count)) {
+                    if (isFirstCondition(aql, counter)) {
                         aql.append(" OR ");
                     }
-                    definesCondition(dc, aql, params, entity, ++count);
+                    definesCondition(dc, aql, params, entity, ++counter);
                 }
                 return;
             case NOT:
                 DocumentCondition documentCondition = document.get(DocumentCondition.class);
                 aql.append(" NOT ");
-                definesCondition(documentCondition, aql, params, entity, ++count);
+                definesCondition(documentCondition, aql, params, entity, ++counter);
                 return;
             default:
                 throw new IllegalArgumentException("The condition does not support in AQL: " + condition.getCondition());
@@ -164,8 +167,8 @@ final class AQLUtils {
         return count > 0 && !FILTER.equals(aql.substring(aql.length() - 8));
     }
 
-    private static void appendCondtion(StringBuilder aql, Map<String, Object> params, char entity, Document document,
-                                       String condition) {
+    private static void appendCondition(StringBuilder aql, Map<String, Object> params,
+                                        char entity, Document document, String condition) {
         String nameParam = getNameParam(document.getName());
         aql.append(" ").append(entity).append('.').append(document.getName())
                 .append(condition).append('@').append(nameParam);
