01563bde5f9715f761a474503345f572ed7abe60
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-20 18:46:30 -0300

    Optimizes factory

--- couchbase-driver/src/main/java/org/jnosql/diana/couchbase/key/CouchbaseBucketManagerFactory.java
+++ couchbase-driver/src/main/java/org/jnosql/diana/couchbase/key/CouchbaseBucketManagerFactory.java
@@ -16,80 +16,9 @@ package org.jnosql.diana.couchbase.key;
 
 import org.jnosql.diana.api.key.BucketManagerFactory;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
-
 /**
- * The couchbase implementation of {@link BucketManagerFactory}. That has support to
- * {@link BucketManagerFactory#getBucketManager(String)} and also the structure {@link Map}, {@link Set},
- * {@link Queue}, {@link List}. Each structure has this specific implementation.
- * <p>{@link CouchbaseList}</p>
- * <p>{@link CouchbaseSet}</p>
- * <p>{@link CouchbaseQueue}</p>
- * <p>{@link CouchbaseMap}</p>
  * The default implementation creates the particular structure with the bucket name as the key.
  */
 public interface CouchbaseBucketManagerFactory extends BucketManagerFactory<CouchbaseBucketManager> {
 
-    /**
-     * Creates a {@link Queue} from bucket name
-     *
-     * @param bucketName a bucket name
-     * @param clazz      the value class
-     * @param key        key to the queue
-     * @param <T>        the value type
-     * @return a {@link Queue} instance
-     * @throws UnsupportedOperationException when the database does not have to it
-     * @throws NullPointerException          when either bucketName or class are null
-     */
-    <T> Queue<T> getQueue(String bucketName, String key, Class<T> clazz) throws UnsupportedOperationException,
-            NullPointerException;
-
-    /**
-     * Creates a {@link Set} from bucket name
-     *
-     * @param bucketName a bucket name
-     * @param clazz      the valeu class
-     * @param key        key to the set
-     * @param <T>        the value type
-     * @return a {@link Set} instance
-     * @throws UnsupportedOperationException when the database does not have to it
-     * @throws NullPointerException          when either bucketName or class are null
-     */
-    <T> Set<T> getSet(String bucketName, String key, Class<T> clazz) throws UnsupportedOperationException,
-            NullPointerException;
-
-
-    /**
-     * Creates a {@link List} from bucket name
-     *
-     * @param bucketName a bucket name
-     * @param clazz      the valeu class
-     * @param key        key to the List
-     * @param <T>        the value type
-     * @return a {@link List} instance
-     * @throws UnsupportedOperationException when the database does not have to it
-     * @throws NullPointerException          when either bucketName or class are null
-     */
-    <T> List<T> getList(String bucketName, String key, Class<T> clazz) throws UnsupportedOperationException,
-            NullPointerException;
-
-
-    /**
-     * Creates a {@link  Map} from bucket name
-     *
-     * @param bucketName the bucket name
-     * @param key        key to the Map
-     * @param keyValue   the key class
-     * @param valueValue the value class
-     * @param <K>        the key type
-     * @param <V>        the value type
-     * @return a {@link Map} instance
-     * @throws UnsupportedOperationException when the database does not have to it
-     * @throws NullPointerException          when either bucketName or class are null
-     */
-    <K, V> Map<K, V> getMap(String bucketName, String key, Class<K> keyValue, Class<V> valueValue) throws
-            UnsupportedOperationException, NullPointerException;
 }
