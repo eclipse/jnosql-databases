f4b05ba80647858c74d6a37771c7fd34c6774b6b
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-20 22:58:09 -0300

    fixes contains

--- couchbase-driver/src/test/java/org/jnosql/diana/couchbase/key/CouchbaseSetTest.java
+++ couchbase-driver/src/test/java/org/jnosql/diana/couchbase/key/CouchbaseSetTest.java
@@ -22,14 +22,22 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CouchbaseSetTest {
 
     private BucketManagerFactory keyValueEntityManagerFactory;
-    private User userOtavioJava = new User("otaviojava");
+    private User otavio = new User("otaviojava");
     private User felipe = new User("ffrancesquini");
     private Set<User> users;
 
@@ -51,10 +59,10 @@ public class CouchbaseSetTest {
     @Test
     public void shouldAddUsers() {
         assertTrue(users.isEmpty());
-        users.add(userOtavioJava);
+        users.add(otavio);
         assertTrue(users.size() == 1);
 
-        users.remove(userOtavioJava);
+        users.remove(otavio);
         assertTrue(users.isEmpty());
     }
 
@@ -63,23 +71,84 @@ public class CouchbaseSetTest {
     @Test
     public void shouldIterate() {
 
-        users.add(userOtavioJava);
-        users.add(userOtavioJava);
+        users.add(otavio);
+        users.add(otavio);
         users.add(felipe);
-        users.add(userOtavioJava);
+        users.add(otavio);
         users.add(felipe);
         int count = 0;
         for (User user : users) {
             count++;
         }
         assertTrue(count == 2);
-        users.remove(userOtavioJava);
+        users.remove(otavio);
         users.remove(felipe);
         count = 0;
         for (User user : users) {
             count++;
         }
         assertTrue(count == 0);
+    }
+
+    @Test
+    public void shouldContains() {
+        users.add(otavio);
+        assertTrue(users.contains(otavio));
+        assertFalse(users.contains(felipe));
+    }
+
+    @Test
+    public void shouldContainsAll() {
+        users.add(otavio);
+        assertTrue(users.containsAll(singleton(otavio)));
+        assertFalse(users.contains(singleton(felipe)));
+    }
+
+    @Test
+    public void shouldRemove() {
+        users.add(otavio);
+        assertTrue(users.remove(otavio));
+        assertFalse(users.remove(felipe));
+    }
+
+    @Test
+    public void shouldRemoveAll() {
+        users.add(otavio);
+        assertTrue(users.removeAll(singleton(otavio)));
+    }
+
+
+    @Test
+    public void shouldAddAll() {
+        assertTrue(users.isEmpty());
+        users.addAll(Arrays.asList(otavio, felipe));
+        assertEquals(2, users.size());
+    }
+
+    @Test
+    public void shouldRetains(){
+        users.addAll(asList(otavio));
+        List<User> newUsers = new ArrayList<>();
+        newUsers.add(otavio);
+        newUsers.add(new User("gama"));
+        users.retainAll(newUsers);
+        assertEquals(1, newUsers.size());
+    }
+
+    @Test
+    public void shouldToArray() {
+        users.addAll(asList(otavio, felipe));
+        Object[] objects = users.toArray();
+        assertEquals(2, objects.length);
+        assertTrue(Stream.of(objects).allMatch(User.class::isInstance));
+    }
+
+    @Test
+    public void shouldToArrayParams() {
+        users.addAll(asList(otavio, felipe));
+        User[] objects = users.toArray(new User[2]);
+        assertEquals(2, objects.length);
+        assertTrue(Stream.of(objects).allMatch(User.class::isInstance));
     }
 
     @AfterEach
