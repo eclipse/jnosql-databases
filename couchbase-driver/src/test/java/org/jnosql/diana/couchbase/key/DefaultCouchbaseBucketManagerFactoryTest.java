6132b69022bcb634c1b391ee2ac45b64c7847680
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-21 08:31:15 -0300

    adds map factory scenarios

--- couchbase-driver/src/test/java/org/jnosql/diana/couchbase/key/DefaultCouchbaseBucketManagerFactoryTest.java
+++ couchbase-driver/src/test/java/org/jnosql/diana/couchbase/key/DefaultCouchbaseBucketManagerFactoryTest.java
@@ -14,6 +14,8 @@
  */
 package org.jnosql.diana.couchbase.key;
 
+import com.couchbase.client.java.datastructures.collections.CouchbaseArrayList;
+import com.couchbase.client.java.datastructures.collections.CouchbaseArraySet;
 import org.jnosql.diana.api.key.BucketManager;
 import org.jnosql.diana.couchbase.CouchbaseUtil;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +23,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DefaultCouchbaseBucketManagerFactoryTest {
 
@@ -41,5 +44,42 @@ public class DefaultCouchbaseBucketManagerFactoryTest {
     @Test
     public void shouldReturnError() {
         assertThrows(NullPointerException.class, () -> factory.getBucketManager(null));
+    }
+
+
+    @Test
+    public void shouldReturnList() {
+        assertTrue(factory.getList("jnosql", String.class) instanceof CouchbaseArrayList);
+        assertTrue(factory.getList("jnosql", User.class) instanceof CouchbaseList);
+        assertTrue(factory.getList("jnosql", "jnosql", User.class) instanceof CouchbaseList);
+        assertTrue(factory.getList("jnosql", "jnosql", String.class) instanceof CouchbaseArrayList);
+    }
+
+    @Test
+    public void shouldReturnSet() {
+        assertTrue(factory.getSet("jnosql", String.class) instanceof CouchbaseArraySet);
+        assertTrue(factory.getSet("jnosql", User.class) instanceof CouchbaseSet);
+        assertTrue(factory.getSet("jnosql", "jnosql", User.class) instanceof CouchbaseSet);
+        assertTrue(factory.getSet("jnosql", "jnosql", String.class) instanceof CouchbaseArraySet);
+    }
+
+    @Test
+    public void shouldReturnQueue() {
+        assertTrue(factory.getQueue("jnosql", String.class) instanceof
+                com.couchbase.client.java.datastructures.collections.CouchbaseQueue);
+        assertTrue(factory.getQueue("jnosql", User.class) instanceof CouchbaseQueue);
+        assertTrue(factory.getQueue("jnosql", "jnosql", User.class) instanceof CouchbaseQueue);
+        assertTrue(factory.getQueue("jnosql", "jnosql", String.class) instanceof
+                com.couchbase.client.java.datastructures.collections.CouchbaseQueue);
+    }
+
+    @Test
+    public void shouldReturnMap() {
+        assertTrue(factory.getMap("jnosql", String.class, String.class) instanceof
+                com.couchbase.client.java.datastructures.collections.CouchbaseMap);
+        assertTrue(factory.getMap("jnosql", String.class, User.class) instanceof CouchbaseMap);
+        assertTrue(factory.getMap("jnosql", "jnosql", String.class, String.class) instanceof
+                com.couchbase.client.java.datastructures.collections.CouchbaseMap);
+        assertTrue(factory.getMap("jnosql", "jnosql", String.class, User.class) instanceof CouchbaseArrayList);
     }
 }
