ed7581c43a7ce416c38cc5c4918161da675a0787
Otavio Santana <otaviopolianasantana@gmail.com>
2018-07-07 07:25:05 -0300

    adds nitial structure

---
+++ couchdb-driver/src/main/java/org/jnosql/diana/couchdb/document/CouchDBDocumentConfiguration.java
@@ -0,0 +1,25 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.couchdb.document;
+
+import org.jnosql.diana.api.document.DocumentCollectionManager;
+import org.jnosql.diana.api.document.DocumentConfiguration;
+
+public class CouchDBDocumentConfiguration implements DocumentConfiguration<CouchDBDocumentCollectionManager> {
+    
+    private static final String FILE_CONFIGURATION = "diana-couchdb.properties";
+}
