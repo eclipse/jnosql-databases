9972e6f0275ddc7b539e31a7048e59daf6a4d78c
Otavio Santana <otaviopolianasantana@gmail.com>
2018-07-08 07:37:27 -0300

    adds tests

---
+++ couchdb-driver/src/test/java/org/jnosql/diana/couchdb/document/DefaultCouchDBDocumentCollectionManagerTest.java
@@ -0,0 +1,68 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.couchdb.document;
+
+import org.jnosql.diana.api.document.Document;
+import org.jnosql.diana.api.document.DocumentEntity;
+import org.jnosql.diana.api.document.Documents;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultCouchDBDocumentCollectionManagerTest {
+
+    public static final String COLLECTION_NAME = "person";
+
+    private CouchDBDocumentCollectionManager entityManager;
+
+    {
+        CouchDBDocumentConfiguration configuration = new CouchDBDocumentConfiguration();
+        CouchDBDocumentCollectionManagerFactory managerFactory = configuration.get();
+        entityManager = managerFactory.get("people");
+    }
+
+    @Test
+    public void shouldInsert() {
+        DocumentEntity entity = getEntity();
+        DocumentEntity documentEntity = entityManager.insert(entity);
+        assertEquals(entity, documentEntity);
+    }
+
+    @Test
+    public void shouldInsert() {
+        DocumentEntity entity = getEntity();
+        DocumentEntity documentEntity = entityManager.insert(entity);
+        assertEquals(entity, documentEntity);
+    }
+
+
+    private DocumentEntity getEntity() {
+        DocumentEntity entity = DocumentEntity.of(COLLECTION_NAME);
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Poliana");
+        map.put("city", "Salvador");
+        map.put("_id", "id");
+
+        List<Document> documents = Documents.of(map);
+        documents.forEach(entity::add);
+        return entity;
+    }
+}
