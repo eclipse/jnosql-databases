885ab5392404801531f86026fd97266504dddfef
Otavio Santana <otaviopolianasantana@gmail.com>
2018-07-11 13:14:24 -0300

    fixes

---
+++ couchdb-driver/src/main/java/org/jnosql/diana/couchdb/document/CouchDBDocumentQuery.java
@@ -0,0 +1,37 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.couchdb.document;
+
+import org.jnosql.diana.api.document.DocumentQuery;
+
+import java.util.Optional;
+
+/**
+ * A CouchDB specialization of DocumentQuery that allows query with bookmark which can do pagination.
+ */
+public interface CouchDBDocumentQuery extends DocumentQuery {
+
+    /**
+     * The A string that enables you to specify which page of results you require. Used for paging
+     * through result sets. Every query returns an opaque string under the bookmark key that can
+     * then be passed back in a query to get the next page of results. If any part of the selector query
+     * changes between requests, the results are undefined. Optional, default: null
+     *
+     * @return the bookmark
+     */
+    Optional<String> getBookmark();
+}
