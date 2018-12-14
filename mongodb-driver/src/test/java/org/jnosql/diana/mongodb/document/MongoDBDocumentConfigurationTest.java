8c25c8134d0eeae3405e601bb9e8563db30325be
rhuanrocha <rhuan080@gmail.com>
2018-12-14 16:05:04 -0200

    Created a new method to MongoDBDocumentConfiguration to allow define another file properties to config the access to MongoDB. Then we can define the file properties with this line: configuration.get(jnosql-config.properties);
    Signed-off-by: rhuanrocha <rhuan080@gmail.com>

--- mongodb-driver/src/test/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfigurationTest.java
+++ mongodb-driver/src/test/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfigurationTest.java
@@ -18,6 +18,7 @@ package org.jnosql.diana.mongodb.document;
 import org.jnosql.diana.api.Settings;
 import org.jnosql.diana.api.document.DocumentCollectionManagerFactory;
 import org.jnosql.diana.api.document.DocumentConfiguration;
+import org.jnosql.diana.driver.ConfigurationReader;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -46,8 +47,16 @@ public class MongoDBDocumentConfigurationTest {
         assertNotNull(configuration.getAsync(settings));
     }
 
+
     @Test
     public void shouldCreateDocumentCollectionManagerFactoryByFile() {
+        DocumentConfiguration configuration = new MongoDBDocumentConfiguration();
+        DocumentCollectionManagerFactory managerFactory = ((MongoDBDocumentConfiguration)configuration).get("mongodb-conf.properties");
+        assertNotNull(managerFactory);
+    }
+
+    @Test
+    public void shouldCreateDocumentCollectionManagerFactoryByDefaultFile() {
         DocumentConfiguration configuration = new MongoDBDocumentConfiguration();
         DocumentCollectionManagerFactory managerFactory = configuration.get();
         assertNotNull(managerFactory);
