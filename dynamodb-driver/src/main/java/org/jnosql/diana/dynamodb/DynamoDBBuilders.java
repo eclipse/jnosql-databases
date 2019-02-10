980df22e7c7847377f1adfba2a05bef38b543964
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-10 21:17:22 -0200

    fixes builder

--- dynamodb-driver/src/main/java/org/jnosql/diana/dynamodb/DynamoDBBuilders.java
+++ dynamodb-driver/src/main/java/org/jnosql/diana/dynamodb/DynamoDBBuilders.java
@@ -15,28 +15,28 @@
 
 package org.jnosql.diana.dynamodb;
 
-import static java.util.Optional.ofNullable;
-
 import org.jnosql.diana.api.Settings;
 
+import static java.util.Optional.ofNullable;
+
 final class DynamoDBBuilders {
-	
-	
-	 private static final String ENDPOINT = "dynamodb.endpoint";
-	 private static final String REGION = "dynamodb.region";
-	 private static final String PROFILE = "dynamodb.profile";
-	 private static final String AWS_ACCESSKEY = "dynamodb.awsaccesskey";
-	 private static final String AWS_SECRETACCESS = "dynamodb.secretaccess";
-	 
-	 private DynamoDBBuilders() {
-	 }
-	 
-	 static void load(Settings settings , DynamoDBBuilder dynamoDB) {
-		 ofNullable(settings.get(ENDPOINT)).map(Object::toString).ifPresent(dynamoDB::endpoint);
-	     ofNullable(settings.get(REGION)).map(Object::toString).ifPresent(dynamoDB::region);
-	     ofNullable(settings.get(PROFILE)).map(Object::toString).ifPresent(dynamoDB::profile);
-	     ofNullable(settings.get(AWS_ACCESSKEY)).map(Object::toString).ifPresent(dynamoDB::awsAccessKey);
-	     ofNullable(settings.get(AWS_SECRETACCESS)).map(Object::toString).ifPresent(dynamoDB::awsSecretAccess);
-	 }
-	 
+
+
+    private static final String ENDPOINT = "dynamodb.endpoint";
+    private static final String REGION = "dynamodb.region";
+    private static final String PROFILE = "dynamodb.profile";
+    private static final String AWS_ACCESSKEY = "dynamodb.awsaccesskey";
+    private static final String AWS_SECRET_ACCESS = "dynamodb.secretaccess";
+
+    private DynamoDBBuilders() {
+    }
+
+    static void load(Settings settings, DynamoDBBuilder dynamoDB) {
+        ofNullable(settings.get(ENDPOINT)).map(Object::toString).ifPresent(dynamoDB::endpoint);
+        ofNullable(settings.get(REGION)).map(Object::toString).ifPresent(dynamoDB::region);
+        ofNullable(settings.get(PROFILE)).map(Object::toString).ifPresent(dynamoDB::profile);
+        ofNullable(settings.get(AWS_ACCESSKEY)).map(Object::toString).ifPresent(dynamoDB::awsAccessKey);
+        ofNullable(settings.get(AWS_SECRET_ACCESS)).map(Object::toString).ifPresent(dynamoDB::awsSecretAccess);
+    }
+
 }
