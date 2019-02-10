0d8ecf39aafc4979d81e9b2354737893f4398159
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-10 21:11:27 -0200

    fixes dynamotableutils

--- dynamodb-driver/src/main/java/org/jnosql/diana/dynamodb/DynamoTableUtils.java
+++ dynamodb-driver/src/main/java/org/jnosql/diana/dynamodb/DynamoTableUtils.java
@@ -33,108 +33,108 @@ import static org.jnosql.diana.dynamodb.ConfigurationAmazonEntity.KEY;
 
 public final class DynamoTableUtils {
 
-	private DynamoTableUtils() {
-	}
+    private DynamoTableUtils() {
+    }
 
-	public static KeySchemaElement createKeyElementSchema(Map<String,KeyType> keys) {
-		
-		KeySchemaElement.Builder keySchemaElementBuilder = KeySchemaElement.builder();
-		
-		keys
-		.entrySet()
-		.forEach(
-			es -> {
-				keySchemaElementBuilder.attributeName(es.getKey());
-				keySchemaElementBuilder.keyType(es.getValue());
-			});
-		
-		return keySchemaElementBuilder.build();
-	}
-	
-	public static AttributeDefinition createAttributeDefinition(Map<String,ScalarAttributeType> attributes) {
-		
-		AttributeDefinition.Builder attributeDefinitionBuilder = AttributeDefinition.builder();
+    public static KeySchemaElement createKeyElementSchema(Map<String, KeyType> keys) {
 
-		attributes
-		.entrySet()
-		.forEach(
-			es ->{
-				attributeDefinitionBuilder.attributeName(es.getKey());
-				attributeDefinitionBuilder.attributeType(es.getValue());
-			}
-		 );
-		
-		return attributeDefinitionBuilder.build();
-	}
-	
-	public static ProvisionedThroughput createProvisionedThroughput(Long readCapacityUnits , Long writeCapacityUnit) {
-		
-		ProvisionedThroughput.Builder provisionedThroughputBuilder = ProvisionedThroughput.builder();
-		
-		if(readCapacityUnits != null && readCapacityUnits.longValue() > 0)
-			provisionedThroughputBuilder.readCapacityUnits(readCapacityUnits);
-		else
-			provisionedThroughputBuilder.readCapacityUnits(5l);
-		
-		
-		if(writeCapacityUnit != null && writeCapacityUnit.longValue() > 0)
-			provisionedThroughputBuilder.writeCapacityUnits(writeCapacityUnit);
-		else
-			provisionedThroughputBuilder.writeCapacityUnits(5l);
-		
-		return provisionedThroughputBuilder.build();
-	}
-	
-	public static Map<String, KeyType> createKeyDefinition(){
-		return Collections.singletonMap(KEY,KeyType.HASH);
-	}
-	
-	public static Map<String, ScalarAttributeType> createAttributesType(){
-		return Collections.singletonMap(KEY, ScalarAttributeType.S);
-	}
+        KeySchemaElement.Builder keySchemaElementBuilder = KeySchemaElement.builder();
 
-	public static void manageTables(String tableName, DynamoDbClient client, Long readCapacityUnits , Long writeCapacityUnit) {
+        keys
+                .entrySet()
+                .forEach(
+                        es -> {
+                            keySchemaElementBuilder.attributeName(es.getKey());
+                            keySchemaElementBuilder.keyType(es.getValue());
+                        });
 
-		boolean more_tables = true;
-		String last_name = null;
+        return keySchemaElementBuilder.build();
+    }
 
-		while (more_tables) {
-			try {
-				ListTablesResponse response = null;
-				if (last_name == null) {
-					ListTablesRequest request = ListTablesRequest.builder().build();
-					response = client.listTables(request);
-				} else {
-					ListTablesRequest request = ListTablesRequest.builder().exclusiveStartTableName(last_name).build();
-					response = client.listTables(request);
-				}
+    public static AttributeDefinition createAttributeDefinition(Map<String, ScalarAttributeType> attributes) {
 
-				List<String> table_names = response.tableNames();
+        AttributeDefinition.Builder attributeDefinitionBuilder = AttributeDefinition.builder();
 
-				if (table_names.size() == 0) {
-					createTable(tableName, client,readCapacityUnits ,writeCapacityUnit);
-				} else {
-					last_name = response.lastEvaluatedTableName();
-					if (last_name == null) {
-						more_tables = false;
-					}
-				}
-			} catch (DynamoDbException e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
+        attributes
+                .entrySet()
+                .forEach(
+                        es -> {
+                            attributeDefinitionBuilder.attributeName(es.getKey());
+                            attributeDefinitionBuilder.attributeType(es.getValue());
+                        }
+                );
 
-	private static void createTable(String tableName, DynamoDbClient client , Long readCapacityUnits , Long writeCapacityUnit) {
-		
-		Map<String, KeyType> keyDefinition = createKeyDefinition();
-		Map<String, ScalarAttributeType> attributeDefinition = createAttributesType();
-		
-		client.createTable(CreateTableRequest.builder()
-				.tableName(tableName)
-				.provisionedThroughput(createProvisionedThroughput(readCapacityUnits, writeCapacityUnit))
-				.keySchema(createKeyElementSchema(keyDefinition))
-				.attributeDefinitions(createAttributeDefinition(attributeDefinition))
-				.build());
-	}
+        return attributeDefinitionBuilder.build();
+    }
+
+    public static ProvisionedThroughput createProvisionedThroughput(Long readCapacityUnits, Long writeCapacityUnit) {
+
+        ProvisionedThroughput.Builder provisionedThroughputBuilder = ProvisionedThroughput.builder();
+
+        if (readCapacityUnits != null && readCapacityUnits.longValue() > 0)
+            provisionedThroughputBuilder.readCapacityUnits(readCapacityUnits);
+        else
+            provisionedThroughputBuilder.readCapacityUnits(5l);
+
+
+        if (writeCapacityUnit != null && writeCapacityUnit.longValue() > 0)
+            provisionedThroughputBuilder.writeCapacityUnits(writeCapacityUnit);
+        else
+            provisionedThroughputBuilder.writeCapacityUnits(5l);
+
+        return provisionedThroughputBuilder.build();
+    }
+
+    public static Map<String, KeyType> createKeyDefinition() {
+        return Collections.singletonMap(KEY, KeyType.HASH);
+    }
+
+    public static Map<String, ScalarAttributeType> createAttributesType() {
+        return Collections.singletonMap(KEY, ScalarAttributeType.S);
+    }
+
+    public static void manageTables(String tableName, DynamoDbClient client, Long readCapacityUnits, Long writeCapacityUnit) {
+
+        boolean more_tables = true;
+        String last_name = null;
+
+        while (more_tables) {
+            try {
+                ListTablesResponse response = null;
+                if (last_name == null) {
+                    ListTablesRequest request = ListTablesRequest.builder().build();
+                    response = client.listTables(request);
+                } else {
+                    ListTablesRequest request = ListTablesRequest.builder().exclusiveStartTableName(last_name).build();
+                    response = client.listTables(request);
+                }
+
+                List<String> table_names = response.tableNames();
+
+                if (table_names.size() == 0) {
+                    createTable(tableName, client, readCapacityUnits, writeCapacityUnit);
+                } else {
+                    last_name = response.lastEvaluatedTableName();
+                    if (last_name == null) {
+                        more_tables = false;
+                    }
+                }
+            } catch (DynamoDbException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void createTable(String tableName, DynamoDbClient client, Long readCapacityUnits, Long writeCapacityUnit) {
+
+        Map<String, KeyType> keyDefinition = createKeyDefinition();
+        Map<String, ScalarAttributeType> attributeDefinition = createAttributesType();
+
+        client.createTable(CreateTableRequest.builder()
+                .tableName(tableName)
+                .provisionedThroughput(createProvisionedThroughput(readCapacityUnits, writeCapacityUnit))
+                .keySchema(createKeyElementSchema(keyDefinition))
+                .attributeDefinitions(createAttributeDefinition(attributeDefinition))
+                .build());
+    }
 }
