70e2a908918ef3390e0fdf7dbbb049ce0ce7ef2b
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-10 21:15:39 -0200

    fixes alingn DynamoDBKeyValueConfiguration

--- dynamodb-driver/src/main/java/org/jnosql/diana/dynamodb/key/DynamoDBKeyValueConfiguration.java
+++ dynamodb-driver/src/main/java/org/jnosql/diana/dynamodb/key/DynamoDBKeyValueConfiguration.java
@@ -17,21 +17,20 @@ package org.jnosql.diana.dynamodb.key;
 import org.jnosql.diana.api.Settings;
 import org.jnosql.diana.api.key.KeyValueConfiguration;
 import org.jnosql.diana.dynamodb.DynamoDBConfiguration;
-
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-public class DynamoDBKeyValueConfiguration extends DynamoDBConfiguration 
-	implements KeyValueConfiguration<DynamoDBBucketManagerFactory> {
+public class DynamoDBKeyValueConfiguration extends DynamoDBConfiguration
+        implements KeyValueConfiguration<DynamoDBBucketManagerFactory> {
 
-	@Override
-	public DynamoDBBucketManagerFactory get() {
-		return new DynamoDBBucketManagerFactory(builder.build());
-	}
+    @Override
+    public DynamoDBBucketManagerFactory get() {
+        return new DynamoDBBucketManagerFactory(builder.build());
+    }
 
-	@Override
-	public DynamoDBBucketManagerFactory get(Settings settings) {
-		DynamoDbClient dynamoDB = getDynamoDB(settings);
-		return new DynamoDBBucketManagerFactory(dynamoDB);
-	}
+    @Override
+    public DynamoDBBucketManagerFactory get(Settings settings) {
+        DynamoDbClient dynamoDB = getDynamoDB(settings);
+        return new DynamoDBBucketManagerFactory(dynamoDB);
+    }
 
 }
