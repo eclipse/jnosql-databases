92e4b521ae4a1c0a8b4b5b757eef3bf33e434039
Otavio Santana <otaviopolianasantana@gmail.com>
2019-04-21 17:42:56 -0300

    fixes ES configuration

--- elasticsearch-driver/src/main/java/org/jnosql/diana/elasticsearch/document/ElasticsearchDocumentConfiguration.java
+++ elasticsearch-driver/src/main/java/org/jnosql/diana/elasticsearch/document/ElasticsearchDocumentConfiguration.java
@@ -29,7 +29,6 @@ import org.jnosql.diana.elasticsearch.ElasticsearchConfigurations;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
