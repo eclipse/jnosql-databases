132ed0527ffe8d30ece404be6e14016331a17f31
Otavio Santana <otaviopolianasantana@gmail.com>
2019-05-03 09:18:44 -0300

    renames configurations to source instead of table

--- mongodb-driver/src/main/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfigurations.java
+++ mongodb-driver/src/main/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfigurations.java
@@ -20,7 +20,7 @@ import java.util.function.Supplier;
 public enum MongoDBDocumentConfigurations implements Supplier<String> {
     HOST("mongodb.host"), USER("mongodb.user"),
     PASSWORD("mongodb.password"),
-    AUTHENTICATION_DATABASE("mongodb.authentication.database"),
+    AUTHENTICATION_SOURCE("mongodb.authentication.source"),
     AUTHENTICATION_MECHANISM("mongodb.authentication.mechanism");
 
     private final String configuration;
