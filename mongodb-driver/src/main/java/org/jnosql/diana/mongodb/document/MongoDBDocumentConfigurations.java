1ed11b3fb9f77215f3935f1a4a1569282fa33391
Otavio Santana <otaviopolianasantana@gmail.com>
2019-05-03 08:07:15 -0300

    defines database authentication

--- mongodb-driver/src/main/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfigurations.java
+++ mongodb-driver/src/main/java/org/jnosql/diana/mongodb/document/MongoDBDocumentConfigurations.java
@@ -18,7 +18,10 @@ package org.jnosql.diana.mongodb.document;
 import java.util.function.Supplier;
 
 public enum MongoDBDocumentConfigurations implements Supplier<String> {
-    HOST("mongodb.host"), USER("mongodb.user"), PASSWORD("mongodb.password"), AUTHENTICATION_DATABASE("mongodb.authentication.table");
+    HOST("mongodb.host"), USER("mongodb.user"),
+    PASSWORD("mongodb.password"),
+    AUTHENTICATION_DATABASE("mongodb.authentication.table"),
+    AUTHENTICATION_MECHANISM("mongodb.authentication.mechanism");
 
     private final String configuration;
 
