105c6eb4a0075920160fa80064d74059a9b2ea94
Otavio Santana <otaviopolianasantana@gmail.com>
2019-06-27 08:26:53 -0300

    creates solr exception

---
+++ solr-driver/src/main/java/org/jnosql/diana/solr/document/SolrException.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.diana.solr.document;
+
+import jakarta.nosql.CommunicationException;
+
+/**
+ * Root of exception on Solr/Jakarta NoSQL integration
+ */
+public class SolrException extends CommunicationException {
+
+    /**
+     * Constructs a new runtime exception with the specified detail message and cause.
+     *
+     * @param message the message
+     * @param cause   the cause
+     */
+    SolrException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
