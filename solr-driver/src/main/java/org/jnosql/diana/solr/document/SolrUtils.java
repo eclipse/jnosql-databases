479eda6080df9f9b2fc0734d95b7f69719043554
Otavio Santana <otaviopolianasantana@gmail.com>
2019-06-27 15:41:13 -0300

    Makes Solr utils simple

--- solr-driver/src/main/java/org/jnosql/diana/solr/document/SolrUtils.java
+++ solr-driver/src/main/java/org/jnosql/diana/solr/document/SolrUtils.java
@@ -23,23 +23,12 @@ import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.jnosql.diana.driver.ValueUtil;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.StreamSupport.stream;
-
 final class SolrUtils {
-    private static final Function<Object, String> KEY_DOCUMENT = d -> cast(d).getName();
-    private static final Function<Object, Object> VALUE_DOCUMENT = d -> SolrUtils.convert(cast(d).getValue());
     private static final String ENTITY = "_entity";
 
     private SolrUtils() {
@@ -54,17 +43,8 @@ final class SolrUtils {
 
     private static Object convert(Value value) {
         Object val = ValueUtil.convert(value);
-        if (val instanceof Document) {
-            jakarta.nosql.document.Document subDocument = (jakarta.nosql.document.Document) val;
-            Object converted = convert(subDocument.getValue());
-            return Collections.singletonMap(subDocument.getName(), converted);
-        }
-        if (isSudDocument(val)) {
-            return getMap(val);
-        }
-        if (isSudDocumentList(val)) {
-            return StreamSupport.stream(Iterable.class.cast(val).spliterator(), false)
-                    .map(SolrUtils::getMap).collect(toList());
+        if (val instanceof Document || isSudDocument(val) || isSudDocumentList(val)) {
+            throw new SolrException("Apache Solr does not support to embedded field");
         }
         return val;
     }
@@ -84,47 +64,10 @@ final class SolrUtils {
                             .orElseThrow(() -> new SolrException("The field _entity is required"));
                     return DocumentEntity.of(entity, documents);
                 }).collect(Collectors.toList());
-//        Predicate<String> isNotNull = s -> values.get(s) != null;
-//        Function<String, jakarta.nosql.document.Document> documentMap = key -> {
-//            Object value = values.get(key);
-//            return getDocument(key, value);
-//        };
-//        return values.keySet().stream().filter(isNotNull).map(documentMap).collect(Collectors.toList());
     }
 
     private static Map<String, Object> solrToMap(Map<String, Object> map) {
         return map.keySet().stream().collect(Collectors.toMap(k -> k, k -> map.get(k)));
-    }
-
-    private static Document getDocument(String key, Object value) {
-        if (value instanceof Document) {
-            //return Document.of(key, of(Document.class.cast(value)));
-        } else if (isDocumentIterable(value)) {
-            List<List<jakarta.nosql.document.Document>> documents = new ArrayList<>();
-            for (Object object : Iterable.class.cast(value)) {
-                Map<?, ?> map = Map.class.cast(object);
-                documents.add(map.entrySet().stream().map(e -> getDocument(e.getKey().toString(), e.getValue())).collect(toList()));
-            }
-            return jakarta.nosql.document.Document.of(key, documents);
-        }
-
-        return Document.of(key, Value.of(value));
-    }
-
-
-    private static boolean isDocumentIterable(Object value) {
-        return value instanceof Iterable &&
-                stream(Iterable.class.cast(value).spliterator(), false)
-                        .allMatch(Document.class::isInstance);
-    }
-
-    private static Object getMap(Object val) {
-        return StreamSupport.stream(Iterable.class.cast(val).spliterator(), false)
-                .collect(toMap(KEY_DOCUMENT, VALUE_DOCUMENT));
-    }
-
-    private static jakarta.nosql.document.Document cast(Object document) {
-        return jakarta.nosql.document.Document.class.cast(document);
     }
 
     private static boolean isSudDocument(Object value) {
