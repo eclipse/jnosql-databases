d0a2069abc98cb24ee39e3ded124d7983508458b
otaviojava <otaviopolianasantana@gmail.com>
2019-10-17 05:54:36 -0300

    creates money type

---
+++ mongodb-driver/src/test/java/org/eclipse/jnosql/diana/mongodb/document/type/Money.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+package org.eclipse.jnosql.diana.mongodb.document.type;
+
+import java.math.BigDecimal;
+
+public class Money {
+
+    private final String currency;
+
+    private final BigDecimal value;
+
+    Money(String currency, BigDecimal value) {
+        this.currency = currency;
+        this.value = value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return currency + ' ' + value;
+    }
+
+    public static Money parse(String text) {
+        String[] texts = text.split(" ");
+        return new Money(texts[0], BigDecimal.valueOf(Double.valueOf(texts[1])));
+    }
+}
