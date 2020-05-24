9cef4f859a5c7601078a554180307ef78746d853
otaviojava <otaviopolianasantana@gmail.com>
2020-05-24 06:41:29 -0300

    adds tck at infinispan

---
+++ infinispan-driver/src/test/java/org/eclipse/jnosql/diana/infinispan/keyvalue/InfinispanBucketManagerSupplier.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2020 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.diana.infinispan.keyvalue;
+
+import jakarta.nosql.keyvalue.BucketManager;
+import jakarta.nosql.keyvalue.BucketManagerFactory;
+import jakarta.nosql.tck.communication.driver.keyvalue.BucketManagerSupplier;
+import org.eclipse.jnosql.diana.infinispan.keyvalue.util.KeyValueEntityManagerFactoryUtils;
+
+public class InfinispanBucketManagerSupplier implements BucketManagerSupplier {
+
+    private static final String BUCKET = "tck-users-entity";
+
+    @Override
+    public BucketManager get() {
+        final BucketManagerFactory factory = KeyValueEntityManagerFactoryUtils.get();
+        return factory.getBucketManager(BUCKET);
+    }
+}
