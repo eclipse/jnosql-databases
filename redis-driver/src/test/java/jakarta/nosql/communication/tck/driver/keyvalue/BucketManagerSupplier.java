96137d8e6809e9a95ef313fcd99a732f61f0a7ed
otaviojava <otaviopolianasantana@gmail.com>
2020-05-24 04:50:46 -0300

    creates BucketManagerSupplier

---
+++ redis-driver/src/test/java/jakarta/nosql/communication/tck/driver/keyvalue/BucketManagerSupplier.java
@@ -0,0 +1,8 @@
+package jakarta.nosql.communication.tck.driver.keyvalue;
+
+import jakarta.nosql.keyvalue.BucketManager;
+
+import java.util.function.Supplier;
+
+public interface BucketManagerSupplier extends Supplier<BucketManager> {
+}
