8f9add24c1d54bef6a4206548f4c35a61189d1f9
otaviojava <otaviopolianasantana@gmail.com>
2020-05-24 08:25:45 -0300

    fixes import

--- hbase-driver/src/main/java/org/eclipse/jnosql/diana/hbase/column/HBaseColumnFamilyManagerFactory.java
+++ hbase-driver/src/main/java/org/eclipse/jnosql/diana/hbase/column/HBaseColumnFamilyManagerFactory.java
@@ -17,8 +17,6 @@ package org.eclipse.jnosql.diana.hbase.column;
 
 import jakarta.nosql.column.ColumnFamilyManagerFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.ColumnFamilyDescriptor;
