167e1ec3353ce22cf3704f789cad14c4d1e70c18
otaviojava <otaviopolianasantana@gmail.com>
2020-05-24 04:47:59 -0300

    creates keyvalue-configuration

--- redis-driver/src/test/java/jakarta/nosql/communication/tck/driver/keyvalue/KeyValueConfigurationTest.java
+++ redis-driver/src/test/java/jakarta/nosql/communication/tck/driver/keyvalue/KeyValueConfigurationTest.java
@@ -1,4 +1,25 @@
 package jakarta.nosql.communication.tck.driver.keyvalue;
 
+import jakarta.nosql.keyvalue.KeyValueConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class KeyValueConfigurationTest {
+
+
+    @Test
+    public void shouldReturnFromConfiguration() {
+        KeyValueConfiguration configuration = KeyValueConfiguration.getConfiguration();
+        assertNotNull(configuration);
+        Assertions.assertTrue(configuration instanceof KeyValueConfiguration);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenParameterIsNull() {
+        KeyValueConfiguration configuration = KeyValueConfiguration.getConfiguration();
+        assertThrows(NullPointerException.class, ()-> configuration.get(null));
+    }
 }
