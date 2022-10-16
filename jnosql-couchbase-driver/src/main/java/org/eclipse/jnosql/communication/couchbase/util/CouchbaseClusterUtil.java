7ed7dcb579330e59cd20665f9b7bdc239371185e
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-16 08:31:00 +0100

    update couchbasecuster util
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-couchbase-driver/src/main/java/org/eclipse/jnosql/communication/couchbase/util/CouchbaseClusterUtil.java
+++ jnosql-couchbase-driver/src/main/java/org/eclipse/jnosql/communication/couchbase/util/CouchbaseClusterUtil.java
@@ -26,16 +26,6 @@ public final class CouchbaseClusterUtil {
         Objects.requireNonNull(database, "database is required");
         CouchbaseCluster authenticate = couchbaseCluster.authenticate(user, password);
 
-       /*
-        ClusterManager clusterManager = authenticate.clusterManager();
-        List<BucketSettings> buckets = clusterManager.getBuckets();
-        if(buckets.stream().noneMatch(b -> b.name().equals(database))) {
-            BucketSettings bucketSettings =  DefaultBucketSettings.builder().name(database).quota(100);
-            clusterManager.insertBucket(bucketSettings);
-            Bucket bucket = authenticate.openBucket(database);
-            bucket.query(N1qlQuery.simple("CREATE PRIMARY INDEX index_" + database + " on " + database));
-            bucket.close();
-        }*/
         return authenticate;
     }
 
