47294bcce8b2d01a4aa75981f712eef9105d134f
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-20 11:06:35 +0200

    Fix typo on couchbase document collection
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-couchbase-driver/src/test/java/org/eclipse/jnosql/communication/couchbase/document/CouchbaseDocumentCollectionManagerFactoryTest.java
+++ jnosql-couchbase-driver/src/test/java/org/eclipse/jnosql/communication/couchbase/document/CouchbaseDocumentCollectionManagerFactoryTest.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class CouhbaseDocumentCollectionManagerFactoryTest {
+public class CouchbaseDocumentCollectionManagerFactoryTest {
 
     private CouchbaseDocumentConfiguration configuration;
 
