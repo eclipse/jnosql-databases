3f6692f30a0faf713f81e02c2aa4c86e3f2bfa6a
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-27 15:04:58 +0100

    Update infinispan-driver to new test API
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-infinispan-driver/src/test/java/org/eclipse/jnosql/communication/infinispan/keyvalue/KeyValueEntityManagerTest.java
+++ jnosql-infinispan-driver/src/test/java/org/eclipse/jnosql/communication/infinispan/keyvalue/KeyValueEntityManagerTest.java
@@ -19,6 +19,7 @@ import jakarta.nosql.Value;
 import jakarta.nosql.keyvalue.BucketManager;
 import jakarta.nosql.keyvalue.BucketManagerFactory;
 import jakarta.nosql.keyvalue.KeyValueEntity;
+import org.assertj.core.api.Assertions;
 import org.eclipse.jnosql.communication.infinispan.keyvalue.util.KeyValueEntityManagerFactoryUtils;
 import org.eclipse.jnosql.communication.infinispan.keyvalue.model.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,8 +31,7 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static java.util.Arrays.asList;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -111,7 +111,11 @@ public class KeyValueEntityManagerTest {
         keyValueEntityManager.put(asList(keyValueSoro, keyValueOtavio));
         List<String> keys = asList("otavio", "soro");
         Iterable<Value> values = keyValueEntityManager.get(keys);
-        assertThat(StreamSupport.stream(values.spliterator(), false).map(value -> value.get(User.class)).collect(Collectors.toList()), containsInAnyOrder(userOtavio, userSoro));
+        assertThat(StreamSupport.stream(values.spliterator(),
+                false)
+                .map(value -> value.get(User.class))
+                .collect(Collectors.toList()))
+                .contains(userOtavio, userSoro);
         keyValueEntityManager.delete(keys);
         Iterable<Value> users = values;
         assertEquals(0L, StreamSupport.stream(keyValueEntityManager.get(keys).spliterator(), false).count());
