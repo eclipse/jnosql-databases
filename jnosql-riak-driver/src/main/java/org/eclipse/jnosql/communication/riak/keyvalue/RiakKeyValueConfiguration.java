74d1521641009e5adb15dbd75fdee1b47a6430a6
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-27 15:46:01 +0100

    impreove riak-driver api
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-riak-driver/src/main/java/org/eclipse/jnosql/communication/riak/keyvalue/RiakKeyValueConfiguration.java
+++ jnosql-riak-driver/src/main/java/org/eclipse/jnosql/communication/riak/keyvalue/RiakKeyValueConfiguration.java
@@ -55,7 +55,7 @@ public class RiakKeyValueConfiguration implements KeyValueConfiguration {
         Map<String, String> properties = ConfigurationReader.from(FILE_CONFIGURATION);
         SettingsBuilder builder = Settings.builder();
 
-        properties.entrySet().forEach(e -> builder.put(e.getKey(), e.getValue()));
+        properties.forEach((key, value) -> builder.put(key, value));
 
         Settings settings = builder.build();
 
