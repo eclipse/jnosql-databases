d5fc52e04dde7c63a7cc0cfa432c40bbd08aa371
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-05 20:58:05 +0000

    add documentation at ArangoDBConfigurations
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-arangodb-driver/src/main/java/org/eclipse/jnosql/communication/arangodb/ArangoDBConfigurations.java
+++ jnosql-arangodb-driver/src/main/java/org/eclipse/jnosql/communication/arangodb/ArangoDBConfigurations.java
@@ -14,25 +14,60 @@
  */
 package org.eclipse.jnosql.communication.arangodb;
 
+
 import java.util.function.Supplier;
 
 /**
  * The settings option available at AragonDB
+ *
+ * @see jakarta.nosql.Settings
  */
 public enum ArangoDBConfigurations implements Supplier<String> {
 
+
+    /**
+     * The database host, where you need to put the port split by colons. E.g., localhost:8529
+     */
     HOST("arangodb.host"),
+    /**
+     * The user credential.
+     */
     USER("arangodb.user"),
+    /**
+     * the password's credential
+     */
     PASSWORD("arangodb.password"),
+    /**
+     * the connection and request timeout in milliseconds.
+     */
     TIMEOUT("arangodb.timeout"),
+    /**
+     * the chunk size when {@link com.arangodb.Protocol#VST} is used.
+     */
     CHUCK_SIZE("arangodb.chuck.size"),
+    /**
+     * The true SSL will be used when connecting to an ArangoDB server.
+     */
     USER_SSL("arangodb.user.ssl"),
+    /**
+     *The {@link com.arangodb.entity.LoadBalancingStrategy}
+     */
     LOAD_BALANCING("arangodb.load.balancing.strategy"),
+    /**
+     * The {@link com.arangodb.Protocol}
+     */
     PROTOCOL("arangodb.protocol"),
+    /**
+     * The maximum number of connections the built in connection pool will open per host.
+     */
     MAX_CONNECTIONS("arangodb.connections.max"),
+    /**
+     *Set host split by comma
+     */
     HOST_LIST("arangodb.acquire.host.list"),
     FILE_CONFIGURATION("diana-arangodb.properties");
     private final String configuration;
+
     ArangoDBConfigurations(String configuration) {
         this.configuration = configuration;
     }
