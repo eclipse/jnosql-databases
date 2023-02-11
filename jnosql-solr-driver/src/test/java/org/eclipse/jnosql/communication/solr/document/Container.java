d0451f85295f12484446e302f4564294ff803431
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-11 05:50:32 +0000

    Create Container
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-solr-driver/src/test/java/org/eclipse/jnosql/communication/solr/document/Container.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.communication.solr.document;
+
+import org.testcontainers.containers.SolrContainer;
+
+public enum Container {
+    INSTANCE;
+
+    private static final String SOLR_IMAGE = "solr:9.1.1";
+    private final SolrContainer container;
+
+    {
+        container = new SolrContainer(SOLR_IMAGE);
+        container.start();
+    }
+
+    public String getHost() {
+        return "http://" + container.getHost() + ":" + container.getSolrPort() + "/solr";
+    }
+}
