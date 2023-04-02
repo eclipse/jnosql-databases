2e76066266c9cb33a6152e913433520a53170680
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-02 16:30:55 +0100

    refactor: update cassandra package name
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-cassandra/src/main/java/org/eclipse/jnosql/databases/cassandra/communication/Relations.java
@@ -0,0 +1,93 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.cassandra.communication;
+
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.relation.Relation;
+import com.datastax.oss.driver.api.querybuilder.term.Term;
+import org.eclipse.jnosql.communication.Condition;
+import org.eclipse.jnosql.communication.TypeReference;
+import org.eclipse.jnosql.communication.Value;
+import org.eclipse.jnosql.communication.column.Column;
+import org.eclipse.jnosql.communication.column.ColumnCondition;
+import org.eclipse.jnosql.communication.driver.ValueUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
+
+final class Relations {
+
+    private Relations() {
+    }
+
+    static List<Relation> createClause(ColumnCondition columnCondition) {
+        if (Objects.isNull(columnCondition)) {
+            return Collections.emptyList();
+        }
+
+        List<Relation> relations = new ArrayList<>();
+        load(columnCondition, relations);
+        return relations;
+    }
+
+    private static void load(ColumnCondition columnCondition, List<Relation> relations) {
+
+        Column column = columnCondition.column();
+        Condition condition = columnCondition.condition();
+
+        switch (condition) {
+            case EQUALS:
+                relations.add(Relation.column(QueryUtils.getName(column)).isEqualTo(getTerm(column)));
+                return;
+            case GREATER_THAN:
+                relations.add(Relation.column(QueryUtils.getName(column)).isGreaterThan(getTerm(column)));
+                return;
+            case GREATER_EQUALS_THAN:
+                relations.add(Relation.column(QueryUtils.getName(column)).isGreaterThanOrEqualTo(getTerm(column)));
+                return;
+            case LESSER_THAN:
+                relations.add(Relation.column(QueryUtils.getName(column)).isLessThan(getTerm(column)));
+                return;
+            case LESSER_EQUALS_THAN:
+                relations.add(Relation.column(QueryUtils.getName(column)).isLessThanOrEqualTo(getTerm(column)));
+                return;
+            case IN:
+                relations.add(Relation.column(QueryUtils.getName(column)).in(getIinValue(column.value())));
+                return;
+            case LIKE:
+                relations.add(Relation.column(QueryUtils.getName(column)).like(getTerm(column)));
+                return;
+            case AND:
+                column.get(new TypeReference<List<ColumnCondition>>() {}).forEach(cc -> load(cc, relations));
+                return;
+            case OR:
+            default:
+                throw new UnsupportedOperationException("The columnCondition " + condition +
+                        " is not supported in cassandra column driver");
+        }
+    }
+
+    private static Term getTerm(Column column) {
+        return literal(ValueUtil.convert(column.value()));
+    }
+
+    private static Term[] getIinValue(Value value) {
+        return ValueUtil.convertToList(value).stream().map(QueryBuilder::literal).toArray(Term[]::new);
+    }
+}
---
+++ jnosql-cassandra/src/test/java/org/eclipse/jnosql/databases/cassandra/mapping/model/Artist.java
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.cassandra.mapping.model;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+import org.eclipse.jnosql.mapping.MappedSuperclass;
+
+import java.util.List;
+
+@Entity
+@MappedSuperclass
+public class Artist {
+
+    @Id
+    private long id;
+
+    @Column
+    private String name;
+    @Column
+    private int age;
+    @Column
+    private List<String> phones;
+    @Column
+    private String ignore;
+
+    Artist() {
+    }
+
+    Artist(long id, String name, int age, List<String> phones, String ignore) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phones = phones;
+        this.ignore = ignore;
+    }
+
+
+    public static ArtistBuilder builder() {
+        return new ArtistBuilder();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<String> getPhones() {
+        return phones;
+    }
+
+    public String getIgnore() {
+        return ignore;
+    }
+}
