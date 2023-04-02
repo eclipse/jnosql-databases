9e77c02933f6a2e320de7e801d81a77165583772
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-02 17:34:21 +0100

    refactor: update solr to the package nameclture
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-solr/src/main/java/org/eclipse/jnosql/databases/solr/communication/SolrDocumentConfigurations.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+package org.eclipse.jnosql.databases.solr.communication;
+
+import java.util.function.Supplier;
+
+/**
+ * An enumeration to show the available options to connect to the Solr database.
+ * It implements {@link Supplier}, where its it returns the property name that might be
+ * overwritten by the system environment using Eclipse Microprofile or Jakarta Config API.
+ *
+ * @see org.eclipse.jnosql.communication.Settings
+ */
+public enum SolrDocumentConfigurations implements Supplier<String> {
+    /**
+     * Database's host. E.g.: jnosql.solr.host=http://localhost:8983/solr/
+     */
+    HOST("jnosql.solr.host"),
+    /**
+     * The user's credential.
+     */
+    USER("jnosql.solr.user"),
+    /**
+     * The password's credential
+     */
+    PASSWORD("jnosql.solr.password"),
+    /**
+     * Define if each operation Apache Solr will commit automatically, true by default.
+     */
+    AUTOMATIC_COMMIT("jnosql.solr.automatic.commit");
+
+    private final String configuration;
+
+    SolrDocumentConfigurations(String configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public String get() {
+        return configuration;
+    }
+}
---
+++ jnosql-solr/src/main/java/org/eclipse/jnosql/databases/solr/mapping/package-info.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+/**
+ * OrientDB is an open source NoSQL database management system written in Java. It is a multi-model database,
+ * supporting graph, document, key/value, and object models, but the relationships are managed as in graph databases
+ * with direct connections between records. It supports schema-less, schema-full and schema-mixed modes.
+ * It has a strong security profiling system based on users and roles and supports querying with Gremlin
+ * along with Solr extended for graph traversal. OrientDB uses several indexing mechanisms based
+ * on B-tree and Extendible hashing, the last one is known as "hash index", there are plans to implement
+ * LSM-tree and Fractal tree index based indexes. Each record has Surrogate key which indicates position
+ * of record inside of Array list , links between records are stored either as single value of record's
+ * position stored inside of referrer or as B-tree of record positions (so-called record IDs or RIDs)
+ * which allows fast traversal (with O(1) complexity) of one-to-many relationships and fast addition/removal
+ * of new links. OrientDB is the second most popular graph database according to the DB-Engines graph database ranking.
+ */
+package org.eclipse.jnosql.databases.solr.mapping;
