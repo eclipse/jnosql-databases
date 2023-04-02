79fd906c2732ffc824f89efc53fc1c57f019e61b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-02 09:20:52 +0100

    refactor: update documentation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-cassandra/src/main/java/org/eclipse/jnosql/databases/cassandra/communication/column/IterableUDT.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.cassandra.communication.column;
+
+import org.eclipse.jnosql.communication.TypeSupplier;
+import org.eclipse.jnosql.communication.Value;
+import org.eclipse.jnosql.communication.column.Column;
+
+import java.util.Objects;
+
+/**
+ * On Cassandra, there is the option to a UDT be part of a list. This implementation holds this option.
+ */
+class IterableUDT implements UDT {
+
+    private final String name;
+
+    private final String userType;
+
+    private final Iterable<Iterable<Column>> columns;
+
+    IterableUDT(String name, String userType, Iterable<Iterable<Column>> columns) {
+        this.name = name;
+        this.userType = userType;
+        this.columns = columns;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public Value value() {
+        return Value.of(columns);
+    }
+
+    @Override
+    public <T> T get(Class<T> clazz) throws NullPointerException, UnsupportedOperationException {
+        Objects.requireNonNull(clazz, "clazz is required");
+        return Value.of(columns).get(clazz);
+    }
+
+    @Override
+    public <T> T get(TypeSupplier<T> typeSupplier) throws NullPointerException, UnsupportedOperationException {
+        Objects.requireNonNull(typeSupplier, "typeSupplier is required");
+        return Value.of(columns).get(typeSupplier);
+
+    }
+
+    @Override
+    public Object get() {
+        return columns;
+    }
+
+    @Override
+    public String getUserType() {
+        return userType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof UDT)) {
+            return false;
+        }
+        UDT udt = (UDT) o;
+        return Objects.equals(name, udt.name()) &&
+                Objects.equals(userType, udt.getUserType()) &&
+                Objects.equals(columns, udt.get());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, userType, columns);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UDT{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", userType='").append(userType).append('\'');
+        sb.append(", columns=").append(columns);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ jnosql-cassandra/src/test/java/org/eclipse/jnosql/databases/cassandra/communication/column/CassandraColumnManagerFactoryTest.java
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+package org.eclipse.jnosql.databases.cassandra.communication.column;
+
+import org.eclipse.jnosql.communication.Settings;
+import org.eclipse.jnosql.communication.SettingsBuilder;
+import org.eclipse.jnosql.communication.column.ColumnManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.NAMED;
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.MATCHES;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@EnabledIfSystemProperty(named = NAMED, matches = MATCHES)
+public class CassandraColumnManagerFactoryTest {
+
+    private CassandraColumnManagerFactory subject;
+
+    @BeforeEach
+    public void setUp() {
+        Settings settings = ColumnDatabase.INSTANCE.getSettings();
+        SettingsBuilder builder = Settings.builder();
+        builder.put(CassandraConfigurations.HOST.get() + ".1", settings.get(CassandraConfigurations.HOST.get() + ".1")
+                .get().toString());
+
+        builder.put(CassandraConfigurations.PORT.get(), settings.get(CassandraConfigurations.PORT.get()).get().toString());
+        builder.put(CassandraConfigurations.QUERY.get() + ".1", " CREATE KEYSPACE IF NOT EXISTS newKeySpace WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};");
+        CassandraConfiguration cassandraConfiguration = new CassandraConfiguration();
+        subject = cassandraConfiguration.apply(builder.build());
+    }
+
+    @Test
+    public void shouldReturnErrorWhenSettingsIsNull() {
+        CassandraConfiguration cassandraConfiguration = new CassandraConfiguration();
+        assertThrows(NullPointerException.class, () -> cassandraConfiguration.apply(null));
+
+        assertThrows(NullPointerException.class, () -> cassandraConfiguration.apply(null));
+    }
+
+    @Test
+    public void shouldReturnEntityManager() {
+        ColumnManager columnEntityManager = subject.apply(Constants.KEY_SPACE);
+        assertNotNull(columnEntityManager);
+    }
+
+}
