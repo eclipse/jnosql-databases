88e9842c931bd35bcef99965a4a36f29414cf656
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-02 18:54:59 +0100

    refactor: update redis to include to the new package nameclature
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-redis/src/main/java/org/eclipse/jnosql/databases/redis/communication/DefaultRedisBucketManagerFactory.java
@@ -0,0 +1,99 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.redis.communication;
+
+import org.eclipse.jnosql.communication.driver.JsonbSupplier;
+import redis.clients.jedis.JedisPool;
+
+import jakarta.json.bind.Jsonb;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import static java.util.Objects.requireNonNull;
+
+class DefaultRedisBucketManagerFactory implements RedisBucketManagerFactory {
+
+    private static final Jsonb JSON = JsonbSupplier.getInstance().get();
+
+    private final JedisPool jedisPool;
+
+    DefaultRedisBucketManagerFactory(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
+    }
+
+
+    @Override
+    public RedisBucketManager apply(String bucketName) {
+        requireNonNull(bucketName, "bucket name is required");
+
+        return new RedisBucketManager(bucketName, JSON, jedisPool.getResource());
+    }
+
+    @Override
+    public <T> List<T> getList(String bucketName, Class<T> clazz) {
+        requireNonNull(bucketName, "bucket name is required");
+        requireNonNull(clazz, "Class type is required");
+        return new RedisList<>(jedisPool.getResource(), clazz, bucketName);
+    }
+
+    @Override
+    public <T> Set<T> getSet(String bucketName, Class<T> clazz) {
+        requireNonNull(bucketName, "bucket name is required");
+        requireNonNull(clazz, "Class type is required");
+        return new RedisSet<>(jedisPool.getResource(), clazz, bucketName);
+    }
+
+    @Override
+    public <T> Queue<T> getQueue(String bucketName, Class<T> clazz) {
+        requireNonNull(bucketName, "bucket name is required");
+        requireNonNull(clazz, "Class type is required");
+        return new RedisQueue<>(jedisPool.getResource(), clazz, bucketName);
+    }
+
+    @Override
+    public <K, V> Map<K, V> getMap(String bucketName, Class<K> keyValue, Class<V> valueValue) {
+        requireNonNull(bucketName, "bucket name is required");
+        requireNonNull(valueValue, "Class type is required");
+        return new RedisMap<>(jedisPool.getResource(), keyValue, valueValue, bucketName);
+    }
+
+    @Override
+    public SortedSet getSortedSet(String key) throws NullPointerException {
+        requireNonNull(key, "key is required");
+        return new DefaultSortedSet(jedisPool.getResource(), key);
+    }
+
+    @Override
+    public Counter getCounter(String key) throws NullPointerException {
+        requireNonNull(key, "key is required");
+        return new DefaultCounter(key, jedisPool.getResource());
+    }
+
+
+    @Override
+    public void close() {
+        jedisPool.close();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("RedisBucketManagerFactory{");
+        sb.append("jedisPool=").append(jedisPool);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ jnosql-redis/src/test/java/org/eclipse/jnosql/databases/redis/communication/RedisSetTest.java
@@ -0,0 +1,128 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+package org.eclipse.jnosql.databases.redis.communication;
+
+
+import org.eclipse.jnosql.communication.keyvalue.BucketManagerFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.NAMED;
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.MATCHES;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@EnabledIfSystemProperty(named = NAMED, matches = MATCHES)
+public class RedisSetTest {
+
+    private BucketManagerFactory keyValueEntityManagerFactory;
+    private User userOtavioJava = new User("otaviojava");
+    private User felipe = new User("ffrancesquini");
+    private Set<User> users;
+
+    @BeforeEach
+    public void init() {
+        keyValueEntityManagerFactory = KeyValueDatabase.INSTANCE.get();
+        users = keyValueEntityManagerFactory.getSet("social-media", User.class);
+    }
+
+    @Test
+    public void shouldAddUsers() {
+        users.add(userOtavioJava);
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    public void shouldRemove() {
+        users.add(userOtavioJava);
+        users.add(felipe);
+        users.remove(felipe);
+
+        assertEquals(1, users.size());
+        assertThat(users).isNotIn(felipe);
+   }
+
+    @Test
+    public void shouldRemoveAll() {
+        users.add(userOtavioJava);
+        users.add(felipe);
+        users.removeAll(Arrays.asList(felipe, userOtavioJava));
+
+        assertEquals(0, users.size());
+    }
+
+    @SuppressWarnings("unused")
+    @Test
+    public void shouldIterate() {
+        users.add(userOtavioJava);
+        users.add(userOtavioJava);
+        users.add(felipe);
+        users.add(userOtavioJava);
+        users.add(felipe);
+        int count = 0;
+        for (User user : users) {
+            count++;
+        }
+        assertEquals(2, count);
+    }
+
+    @Test
+    public void shouldContains() {
+        users.add(userOtavioJava);
+        assertTrue(users.contains(userOtavioJava));
+    }
+
+    @Test
+    public void shouldContainsAll() {
+        users.add(userOtavioJava);
+        users.add(felipe);
+        assertTrue(users.containsAll(Arrays.asList(userOtavioJava, felipe)));
+    }
+
+    @Test
+    public void shouldReturnSize() {
+        users.add(userOtavioJava);
+        users.add(felipe);
+        assertEquals(2, users.size());
+    }
+
+    @Test
+    public void shouldClear() {
+        users.add(userOtavioJava);
+        users.add(felipe);
+
+        users.clear();
+        assertTrue(users.isEmpty());
+    }
+
+    @Test
+    public void shouldThrowExceptionRetainAll() {
+        assertThrows(UnsupportedOperationException.class, () -> users.retainAll(Collections.singletonList(userOtavioJava)));
+    }
+
+    @AfterEach
+    public void dispose() {
+        users.clear();
+    }
+}
