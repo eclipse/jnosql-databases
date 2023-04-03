29a9ffbf2d5022a5266f569af0a980d7011f69ba
Maximillian Arruda <dearrudam@gmail.com>
2023-04-02 23:45:42 -0300

    feat: migrated jnosql-arangodb-extension test implementation
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ jnosql-arangodb/src/test/java/org/eclipse/jnosql/databases/arangodb/mapping/document/ArangoDBDocumentRepositoryProxyTest.java
@@ -0,0 +1,102 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.arangodb.mapping.document;
+
+import jakarta.inject.Inject;
+import org.eclipse.jnosql.mapping.Convert;
+import org.eclipse.jnosql.mapping.document.DocumentEntityConverter;
+import org.eclipse.jnosql.mapping.document.query.DocumentRepositoryProducer;
+import org.eclipse.jnosql.mapping.document.spi.DocumentExtension;
+import org.eclipse.jnosql.mapping.keyvalue.KeyValueWorkflow;
+import org.eclipse.jnosql.mapping.keyvalue.spi.KeyValueExtension;
+import org.eclipse.jnosql.mapping.reflection.EntityMetadataExtension;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Proxy;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@EnableAutoWeld
+@AddPackages(value = {Convert.class, KeyValueWorkflow.class,
+        DocumentEntityConverter.class, AQL.class})
+@AddPackages(MockProducer.class)
+@AddExtensions({EntityMetadataExtension.class, KeyValueExtension.class,
+        DocumentExtension.class, ArangoDBExtension.class})
+public class ArangoDBDocumentRepositoryProxyTest {
+
+    private ArangoDBTemplate template;
+
+    @Inject
+    private DocumentRepositoryProducer producer;
+
+    private PersonRepository personRepository;
+
+    @BeforeEach
+    public void setUp() {
+        this.template = Mockito.mock(ArangoDBTemplate.class);
+
+        PersonRepository personRepository = producer.get(PersonRepository.class, template);
+        ArangoDBDocumentRepositoryProxy handler = new ArangoDBDocumentRepositoryProxy(template,
+                PersonRepository.class, personRepository);
+
+        when(template.insert(any(Person.class))).thenReturn(new Person());
+        when(template.insert(any(Person.class), any(Duration.class))).thenReturn(new Person());
+        when(template.update(any(Person.class))).thenReturn(new Person());
+        this.personRepository = (PersonRepository) Proxy.newProxyInstance(PersonRepository.class.getClassLoader(),
+                new Class[]{PersonRepository.class},
+                handler);
+    }
+
+
+    @Test
+    public void shouldFindAll() {
+        personRepository.findAllQuery();
+        verify(template).aql("FOR p IN Person RETURN p", emptyMap());
+    }
+
+    @Test
+    public void shouldFindByNameAQL() {
+        ArgumentCaptor<Map> captor = ArgumentCaptor.forClass(Map.class);
+        personRepository.findByName("Ada");
+        verify(template).aql(eq("FOR p IN Person FILTER p.name = @name RETURN p"), captor.capture());
+
+        Map value = captor.getValue();
+        assertEquals("Ada", value.get("name"));
+    }
+
+    interface PersonRepository extends ArangoDBRepository<Person, String> {
+
+        @AQL("FOR p IN Person RETURN p")
+        List<Person> findAllQuery();
+
+        @AQL("FOR p IN Person FILTER p.name = @name RETURN p")
+        List<Person> findByName(@Param("name") String name);
+    }
+}
