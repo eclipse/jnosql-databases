7d0c0e66ae2869195bb1bed091fe57a2f36e6a0e
Maximillian Arruda <dearrudam@gmail.com>
2023-04-02 23:38:37 -0300

    chore: renamed package from org.eclipse.jnosql.communication to org.eclipse.jnosql.databases
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ jnosql-arangodb/src/main/java/org/eclipse/jnosql/databases/arangodb/communication/ArangoDBConfigurations.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.arangodb.communication;
+
+
+import java.util.function.Supplier;
+
+/**
+ * An enumeration to show the available options to connect to the ArangoDB database.
+ * It implements {@link Supplier}, where its it returns the property name that might be
+ * overwritten by the system environment using Eclipse Microprofile or Jakarta Config API.
+ *
+ * @see org.eclipse.jnosql.communication.Settings
+ */
+public enum ArangoDBConfigurations implements Supplier<String> {
+
+
+    /**
+     * The database host, where you need to put the port split by colons. E.g.: jnosql.arangodb.host=localhost:8529
+     */
+    HOST("jnosql.arangodb.host"),
+    /**
+     * The user's credential.
+     */
+    USER("jnosql.arangodb.user"),
+    /**
+     * The password's credential
+     */
+    PASSWORD("jnosql.arangodb.password"),
+    /**
+     * The connection and request timeout in milliseconds.
+     */
+    TIMEOUT("jnosql.arangodb.timeout"),
+    /**
+     * The chunk size when {@link com.arangodb.Protocol} is used.
+     */
+    CHUCK_SIZE("jnosql.arangodb.chuck.size"),
+    /**
+     * The true SSL will be used when connecting to an ArangoDB server.
+     */
+    USER_SSL("jnosql.arangodb.user.ssl"),
+    /**
+     *The {@link com.arangodb.entity.LoadBalancingStrategy}
+     */
+    LOAD_BALANCING("jnosql.arangodb.load.balancing.strategy"),
+    /**
+     * The {@link com.arangodb.Protocol}
+     */
+    PROTOCOL("jnosql.arangodb.protocol"),
+    /**
+     * The maximum number of connections the built in connection pool will open per host.
+     */
+    MAX_CONNECTIONS("jnosql.arangodb.connections.max"),
+    /**
+     *Set hosts split by comma
+     */
+    HOST_LIST("jnosql.arangodb.acquire.host.list");
+    private final String configuration;
+
+    ArangoDBConfigurations(String configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public String get() {
+        return configuration;
+    }
+}
---
+++ jnosql-arangodb/src/main/java/org/eclipse/jnosql/databases/arangodb/communication/document/ArangoDBUtil.java
@@ -0,0 +1,173 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.arangodb.communication.document;
+
+
+import com.arangodb.ArangoDB;
+import com.arangodb.DbName;
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.entity.CollectionEntity;
+import org.eclipse.jnosql.communication.Value;
+import org.eclipse.jnosql.communication.document.Document;
+import org.eclipse.jnosql.communication.document.DocumentEntity;
+import org.eclipse.jnosql.communication.driver.ValueUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static java.util.Collections.singletonMap;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.StreamSupport.stream;
+
+/**
+ * The utilitarian class to ArangoDB
+ */
+public final class ArangoDBUtil {
+
+    public static final String KEY = "_key";
+    public static final String ID = "_id";
+    public static final String REV = "_rev";
+
+    private static final Function<Object, String> KEY_DOCUMENT = d -> cast(d).name();
+    private static final Function<Object, Object> VALUE_DOCUMENT = d -> ValueUtil.convert(cast(d).value());
+
+
+    private static final Logger LOGGER = Logger.getLogger(ArangoDBUtil.class.getName());
+
+    private static final Function<Map.Entry<?, ?>, Document> ENTRY_DOCUMENT = entry ->
+            Document.of(entry.getKey().toString(), entry.getValue());
+
+    private ArangoDBUtil() {
+    }
+
+
+    static void checkDatabase(String database, ArangoDB arangoDB) {
+        Objects.requireNonNull(database, "database is required");
+        try {
+            Collection<String> databases = arangoDB.getDatabases();
+            if (!databases.contains(database)) {
+                arangoDB.createDatabase(DbName.of(database));
+            }
+        } catch (ArangoDBException e) {
+            LOGGER.log(Level.WARNING, "Failed to create database: " + database, e);
+        }
+    }
+
+    public static void checkCollection(String bucketName, ArangoDB arangoDB, String namespace) {
+        checkDatabase(bucketName, arangoDB);
+        List<String> collections = arangoDB.db(DbName.of(bucketName))
+                .getCollections().stream()
+                .map(CollectionEntity::getName)
+                .collect(toList());
+        if (!collections.contains(namespace)) {
+            arangoDB.db(DbName.of(bucketName)).createCollection(namespace);
+        }
+    }
+
+
+    static DocumentEntity toEntity(BaseDocument document) {
+        Map<String, Object> properties = document.getProperties();
+        List<Document> documents = properties.keySet().stream()
+                .map(k -> toDocument(k, properties))
+                .collect(Collectors.toList());
+
+        documents.add(Document.of(KEY, document.getKey()));
+        documents.add(Document.of(ID, document.getId()));
+        documents.add(Document.of(REV, document.getRevision()));
+        String collection = document.getId().split("/")[0];
+        return DocumentEntity.of(collection, documents);
+    }
+
+    static BaseDocument getBaseDocument(DocumentEntity entity) {
+        Map<String, Object> map = new HashMap<>();
+        for (Document document : entity.documents()) {
+            map.put(document.name(), convert(document.value()));
+        }
+        return new BaseDocument(map);
+    }
+
+    private static Document toDocument(String key, Map<String, Object> properties) {
+        Object value = properties.get(key);
+        if (Map.class.isInstance(value)) {
+            Map map = Map.class.cast(value);
+            return Document.of(key, map.keySet()
+                    .stream().map(k -> toDocument(k.toString(), map))
+                    .collect(Collectors.toList()));
+        }
+        if (isADocumentIterable(value)) {
+            List<List<Document>> documents = new ArrayList<>();
+            for (Object object : Iterable.class.cast(value)) {
+                Map<?, ?> map = Map.class.cast(object);
+                documents.add(map.entrySet().stream().map(ENTRY_DOCUMENT).collect(toList()));
+            }
+            return Document.of(key, documents);
+
+        }
+        return Document.of(key, value);
+    }
+
+    private static boolean isADocumentIterable(Object value) {
+        return Iterable.class.isInstance(value) &&
+                stream(Iterable.class.cast(value).spliterator(), false)
+                        .allMatch(Map.class::isInstance);
+    }
+
+    private static Object convert(Value value) {
+        Object val = ValueUtil.convert(value);
+
+        if (Document.class.isInstance(val)) {
+            Document document = Document.class.cast(val);
+            return singletonMap(document.name(), convert(document.value()));
+        }
+        if (isSudDocument(val)) {
+            return getMap(val);
+        }
+        if (isSudDocumentList(val)) {
+            return StreamSupport.stream(Iterable.class.cast(val).spliterator(), false)
+                    .map(ArangoDBUtil::getMap).collect(toList());
+        }
+        return val;
+    }
+
+    private static Object getMap(Object val) {
+        return StreamSupport.stream(Iterable.class.cast(val).spliterator(), false)
+                .collect(toMap(KEY_DOCUMENT, VALUE_DOCUMENT));
+    }
+
+    private static boolean isSudDocumentList(Object value) {
+        return value instanceof Iterable && StreamSupport.stream(Iterable.class.cast(value).spliterator(), false).
+                allMatch(d -> d instanceof Iterable && isSudDocument(d));
+    }
+
+    private static boolean isSudDocument(Object value) {
+        return value instanceof Iterable && StreamSupport.stream(Iterable.class.cast(value).spliterator(), false).
+                allMatch(org.eclipse.jnosql.communication.document.Document.class::isInstance);
+    }
+
+    private static org.eclipse.jnosql.communication.document.Document cast(Object document) {
+        return org.eclipse.jnosql.communication.document.Document.class.cast(document);
+    }
+
+}
