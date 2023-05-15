d31fdb10a8cc203f4612d8ee2a0024ae662def34
Maximillian Arruda <dearrudam@gmail.com>
2023-05-15 16:23:23 -0300

    chore: remove unnecessary file
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- jnosql-dynamodb/dynamoDockerRun.sh
+++
@@ -1,7 +0,0 @@
-AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
-
-docker run -it --rm  -p 8000:8000 \
-   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-   amazon/dynamodb-local
