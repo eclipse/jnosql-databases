1120ae2c5857ccf0158ebe32a5a764a22c03bd06
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-18 06:08:42 +0100

    chore: create mongodb template integration
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mongodb/src/test/java/org/eclipse/jnosql/databases/mongodb/integration/MongoDBTemplateIntegrationTest.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *   Alessandro Moscatelli
+ */
+package org.eclipse.jnosql.databases.mongodb.integration;
+
+
+import org.eclipse.jnosql.databases.mongodb.mapping.Music;
+import org.eclipse.jnosql.mapping.Convert;
+import org.eclipse.jnosql.mapping.document.DocumentEntityConverter;
+import org.eclipse.jnosql.mapping.document.spi.DocumentExtension;
+import org.eclipse.jnosql.mapping.reflection.EntityMetadataExtension;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+
+@EnableAutoWeld
+@AddPackages(value = {Convert.class, DocumentEntityConverter.class})
+@AddPackages(Music.class)
+@AddExtensions({EntityMetadataExtension.class,
+        DocumentExtension.class})
+class MongoDBTemplateIntegrationTest {
+}
