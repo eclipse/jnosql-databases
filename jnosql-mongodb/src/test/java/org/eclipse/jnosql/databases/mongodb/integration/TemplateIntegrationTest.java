b2226351c4f07fe26873972e403b2e47b996989f
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-18 06:39:11 +0100

    test: create test integration scenario
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mongodb/src/test/java/org/eclipse/jnosql/databases/mongodb/integration/TemplateIntegrationTest.java
+++ jnosql-mongodb/src/test/java/org/eclipse/jnosql/databases/mongodb/integration/TemplateIntegrationTest.java
@@ -15,24 +15,74 @@
 package org.eclipse.jnosql.databases.mongodb.integration;
 
 
-import org.eclipse.jnosql.databases.mongodb.mapping.Music;
+import jakarta.annotation.Priority;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import jakarta.nosql.Template;
+import jakarta.nosql.document.DocumentTemplate;
+import org.eclipse.jnosql.communication.document.DocumentManager;
+import org.eclipse.jnosql.databases.mongodb.communication.DocumentDatabase;
+import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentConfigurations;
+import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentManager;
 import org.eclipse.jnosql.mapping.Convert;
+import org.eclipse.jnosql.mapping.config.MappingConfigurations;
 import org.eclipse.jnosql.mapping.document.DocumentEntityConverter;
 import org.eclipse.jnosql.mapping.document.spi.DocumentExtension;
 import org.eclipse.jnosql.mapping.reflection.EntityMetadataExtension;
+import org.jboss.weld.junit5.auto.AddBeanClasses;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.jboss.weld.junit5.auto.ExcludeBean;
+import org.jboss.weld.junit5.auto.ExcludeBeanClasses;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
+import java.util.Optional;
+
+import static java.util.UUID.randomUUID;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.jnosql.communication.driver.IntegrationTest.MATCHES;
 import static org.eclipse.jnosql.communication.driver.IntegrationTest.NAMED;
+import static org.eclipse.jnosql.databases.mongodb.communication.DocumentDatabase.INSTANCE;
 
 @EnableAutoWeld
 @AddPackages(value = {Convert.class, DocumentEntityConverter.class})
-@AddPackages(Music.class)
+@AddPackages(Book.class)
 @AddExtensions({EntityMetadataExtension.class,
         DocumentExtension.class})
 @EnabledIfSystemProperty(named = NAMED, matches = MATCHES)
-class MongoDBTemplateIntegrationTest {
+class TemplateIntegrationTest {
+
+    @Inject
+    private Template template;
+
+    static {
+        INSTANCE.get("library");
+        System.setProperty(MongoDBDocumentConfigurations.HOST.get() + ".1", INSTANCE.host());
+        System.setProperty(MappingConfigurations.DOCUMENT_DATABASE.get(), "library");
+    }
+
+    @Test
+    public void shouldInsert() {
+        Book book = new Book(randomUUID().toString(), "Effective Java", 1);
+        template.insert(book);
+        Optional<Book> optional = template.find(Book.class, book.id());
+        assertThat(optional).isNotNull().isNotEmpty()
+                .get().isEqualTo(book);
+    }
+
+    @Test
+    public void shouldUpdate() {
+
+    }
+
+    @Test
+    public void shouldFindById() {
+
+    }
+
+
 }
