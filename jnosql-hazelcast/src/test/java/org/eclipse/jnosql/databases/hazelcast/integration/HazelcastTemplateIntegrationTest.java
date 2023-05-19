a9b9e8e44cbcd598ef21d04fae8c8032fe00e00e
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-19 09:59:15 +0100

    fix: update integration test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-hazelcast/src/test/java/org/eclipse/jnosql/databases/hazelcast/integration/HazelcastTemplateIntegrationTest.java
+++ jnosql-hazelcast/src/test/java/org/eclipse/jnosql/databases/hazelcast/integration/HazelcastTemplateIntegrationTest.java
@@ -1,2 +1,91 @@
-package org.eclipse.jnosql.databases.hazelcast.integration;public class HazelcastTemplateIntegrationTest {
+/*
+ *  Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.hazelcast.integration;
+
+
+import jakarta.inject.Inject;
+import org.assertj.core.api.Assertions;
+import org.eclipse.jnosql.communication.Value;
+import org.eclipse.jnosql.databases.hazelcast.communication.model.User;
+import org.eclipse.jnosql.databases.hazelcast.mapping.HazelcastTemplate;
+import org.eclipse.jnosql.mapping.Convert;
+import org.eclipse.jnosql.mapping.keyvalue.KeyValueEntityConverter;
+import org.eclipse.jnosql.mapping.keyvalue.spi.KeyValueExtension;
+import org.eclipse.jnosql.mapping.reflection.EntityMetadataExtension;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import java.util.Optional;
+
+import static java.util.UUID.randomUUID;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.MATCHES;
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.NAMED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@EnableAutoWeld
+@AddPackages(value = {Convert.class, KeyValueEntityConverter.class})
+@AddPackages(Book.class)
+@AddPackages(HazelcastTemplate.class)
+@AddExtensions({EntityMetadataExtension.class,
+        KeyValueExtension.class})
+@EnabledIfSystemProperty(named = NAMED, matches = MATCHES)
+class HazelcastTemplateIntegrationTest {
+
+    @Inject
+    private HazelcastTemplate template;
+
+
+    @Test
+    public void shouldPutValue() {
+        Book book = new Book(randomUUID().toString(), "Effective Java", 1);
+        template.put(book);
+        Optional<Book> effective = template.get("otavio", Book.class);
+        assertThat(effective)
+                .isNotNull()
+                .isPresent()
+                .get().isEqualTo(book);
+    }
+
+    @Test
+    public void shouldGet() {
+        Book book = new Book(randomUUID().toString(), "Effective Java", 1);
+        template.put(book);
+        Optional<Book> effective = template.get("otavio", Book.class);
+        assertThat(effective)
+                .isNotNull()
+                .isPresent()
+                .get().isEqualTo(book);
+    }
+
+    @Test
+    public void shouldDelete() {
+        Book book = new Book(randomUUID().toString(), "Effective Java", 1);
+        template.put(book);
+        Optional<Book> effective = template.get("otavio", Book.class);
+        assertThat(effective)
+                .isNotNull()
+                .isPresent()
+                .get().isEqualTo(book);
+        template.delete(Book.class, book.id());
+
+        assertThat(template.get("otavio", Book.class))
+                .isEmpty();
+    }
 }
