a6d9a133312253ba14937b833664cfbcd9def6a1
Maximillian Arruda <dearrudam@gmail.com>
2023-06-07 02:43:03 -0300

    tests(integration): add Author class for testing purposes
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ jnosql-elasticsearch/src/test/java/org/eclipse/jnosql/databases/elasticsearch/integration/Author.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Maximillian Arruda
+ *
+ */
+
+package org.eclipse.jnosql.databases.elasticsearch.integration;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+
+import java.util.Objects;
+
+@Entity
+public final class Author {
+
+    @Column("name")
+    private final String name;
+
+    public Author(
+            @Column("name")
+            String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Author) obj;
+        return Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Author[" +
+                "name=" + name + ']';
+    }
+
+}
