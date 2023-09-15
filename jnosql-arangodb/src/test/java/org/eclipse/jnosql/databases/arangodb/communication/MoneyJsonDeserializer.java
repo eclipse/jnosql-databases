096001c10e7a45175a921dc126ea474d07d492eb
Otavio Santana <otaviopolianasantana@gmail.com>
2023-09-15 19:14:30 +0100

    test: create serializer context
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-arangodb/src/test/java/org/eclipse/jnosql/databases/arangodb/communication/MoneyJsonDeserializer.java
+++ jnosql-arangodb/src/test/java/org/eclipse/jnosql/databases/arangodb/communication/MoneyJsonDeserializer.java
@@ -5,16 +5,32 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 public class MoneyJsonDeserializer extends JsonDeserializer<Money> {
 
     @Override
     public Money deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
             throws IOException, JacksonException {
-        return null;
+
+        JsonNode rootNode = jsonParser.getCodec().readTree(jsonParser);
+        JsonNode currencyNode = rootNode.get("currency");
+        JsonNode valueNode = rootNode.get("value");
+        String currency = "USD";
+        BigDecimal amount = BigDecimal.ZERO;
+        if (currencyNode != null && currencyNode.isTextual()) {
+            currency = currencyNode.asText();
+        }
+
+        if (valueNode != null && valueNode.isNumber()) {
+            amount = valueNode.decimalValue();
+        }
+
+        return new Money(currency, amount);
     }
 }
