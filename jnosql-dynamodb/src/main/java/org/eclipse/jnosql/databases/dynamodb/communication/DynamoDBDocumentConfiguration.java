1ab5c8e51fd46336aac85100fa206fecd3b0401a
Maximillian Arruda <dearrudam@gmail.com>
2023-12-26 20:30:11 -0300

    feat: add document configuration for dynamodb
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ jnosql-dynamodb/src/main/java/org/eclipse/jnosql/databases/dynamodb/communication/DynamoDBDocumentConfiguration.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Maximillian Arruda
+ */
+
+package org.eclipse.jnosql.databases.dynamodb.communication;
+
+import org.eclipse.jnosql.communication.Settings;
+import org.eclipse.jnosql.communication.document.DocumentConfiguration;
+import org.eclipse.jnosql.communication.document.DocumentManagerFactory;
+
+public class DynamoDBDocumentConfiguration extends DynamoDBConfiguration
+        implements DocumentConfiguration {
+    @Override
+    public DocumentManagerFactory apply(Settings settings) {
+        return null;
+    }
+}
