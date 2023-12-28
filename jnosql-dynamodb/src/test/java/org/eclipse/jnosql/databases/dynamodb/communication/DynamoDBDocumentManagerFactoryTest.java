502ae0f8f6f0774a0205e57edae8978d941141c4
Maximillian Arruda <dearrudam@gmail.com>
2023-12-27 21:42:34 -0300

    test: add tests for DynamoDBDocumentManagerFactory
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ jnosql-dynamodb/src/test/java/org/eclipse/jnosql/databases/dynamodb/communication/DynamoDBDocumentManagerFactoryTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Maximillian Arruda
+ */
+
+package org.eclipse.jnosql.databases.dynamodb.communication;
+
+import org.eclipse.jnosql.communication.document.DocumentManagerFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.MATCHES;
+import static org.eclipse.jnosql.communication.driver.IntegrationTest.NAMED;
+
+@EnabledIfSystemProperty(named = NAMED, matches = MATCHES)
+class DynamoDBDocumentManagerFactoryTest {
+
+    private DocumentManagerFactory documentManagerFactory;
+
+    @BeforeEach
+    void setup() {
+        this.documentManagerFactory = DynamoDBTestUtils.INSTANCE.getDocumentManagerFactory();
+        assertSoftly(softly -> {
+            softly.assertThat(documentManagerFactory).isNotNull();
+            softly.assertThat(documentManagerFactory).isInstanceOf(DynamoDBDocumentManagerFactory.class);
+        });
+    }
+
+    @Test
+    void shouldCreateDocumentManager() {
+        var documentManager = documentManagerFactory.apply("anydatabase");
+        assertSoftly(softly -> {
+            softly.assertThat(documentManager).isNotNull();
+        });
+    }
+
+}
