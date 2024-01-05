949d3fdbdc7cff0f423c296ce1dda18b73ce263d
Maximillian Arruda <dearrudam@gmail.com>
2024-01-05 03:47:29 -0300

    chore: implement and test documentManager.insert() method implementation
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- jnosql-dynamodb/src/main/java/org/eclipse/jnosql/databases/dynamodb/communication/DynamoDBDocumentManager.java
+++ jnosql-dynamodb/src/main/java/org/eclipse/jnosql/databases/dynamodb/communication/DynamoDBDocumentManager.java
@@ -15,29 +15,72 @@
 
 package org.eclipse.jnosql.databases.dynamodb.communication;
 
+import org.eclipse.jnosql.communication.Settings;
 import org.eclipse.jnosql.communication.document.DocumentDeleteQuery;
 import org.eclipse.jnosql.communication.document.DocumentEntity;
 import org.eclipse.jnosql.communication.document.DocumentManager;
 import org.eclipse.jnosql.communication.document.DocumentPreparedStatement;
 import org.eclipse.jnosql.communication.document.DocumentQuery;
+import software.amazon.awssdk.enhanced.dynamodb.AttributeConverterProvider;
+import software.amazon.awssdk.enhanced.dynamodb.AttributeValueType;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableMetadata;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.document.EnhancedDocument;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTimeToLiveRequest;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTimeToLiveResponse;
+import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
+import software.amazon.awssdk.services.dynamodb.model.TimeToLiveStatus;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.Objects.requireNonNull;
+import static org.eclipse.jnosql.databases.dynamodb.communication.DocumentEntityConverter.toEnhancedDocument;
 
 public class DynamoDBDocumentManager implements DocumentManager {
 
     private final String database;
+
+    private final Settings settings;
+
     private final DynamoDbClient dynamoDbClient;
 
-    public DynamoDBDocumentManager(String database, DynamoDbClient dynamoDbClient) {
+    private final DynamoDbEnhancedClient dynamoDbEnhancedClient;
+
+    private final UnaryOperator<String> entityNameResolver;
+
+    private final ConcurrentHashMap<String, DynamoDbTable<EnhancedDocument>> tables = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Supplier<String>> ttlAttributeNamesByTable = new ConcurrentHashMap<>();
+
+    public DynamoDBDocumentManager(String database, DynamoDbClient dynamoDbClient, Settings settings) {
         this.database = database;
         this.dynamoDbClient = dynamoDbClient;
+        this.dynamoDbEnhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build();
+        this.settings = settings;
+        this.entityNameResolver = this::resolveEntityName;
+    }
+
+    private String resolveEntityName(String entityName) {
+        return this.settings.get(DynamoDBConfigurations.ENTITY_PARTITION_KEY, String.class).orElse(entityName);
     }
 
     public DynamoDbClient getDynamoDbClient() {
         return dynamoDbClient;
+    }
+
+    public DynamoDbEnhancedClient getDynamoDbEnhancedClient() {
+        return dynamoDbEnhancedClient;
     }
 
     @Override
@@ -45,24 +88,101 @@ public class DynamoDBDocumentManager implements DocumentManager {
         return database;
     }
 
+    public UnaryOperator<String> getEntityNameResolver() {
+        return this.entityNameResolver;
+    }
+
     @Override
     public DocumentEntity insert(DocumentEntity documentEntity) {
-        return null;
+        requireNonNull(documentEntity, "documentEntity is required");
+        var enhancedDocument = convertToEnhancedDocument(documentEntity);
+        getTableFor(documentEntity.name())
+                .putItem(enhancedDocument);
+        return documentEntity;
+    }
+
+    private EnhancedDocument convertToEnhancedDocument(DocumentEntity documentEntity) {
+        return toEnhancedDocument(getEntityNameResolver(), documentEntity);
+    }
+
+    private DynamoDbTable<EnhancedDocument> getTableFor(String name) {
+        return this.tables.computeIfAbsent(name, this::getOrCreateTable);
+    }
+
+    private Supplier<String> getTTLAttributeNameFor(String name) {
+        return this.ttlAttributeNamesByTable.computeIfAbsent(name, this::getOrCreateTableWithTTLSupport);
+    }
+
+    private Supplier<String> getOrCreateTableWithTTLSupport(String nameKey) {
+        DynamoDbTable<EnhancedDocument> table = this.getOrCreateTable(nameKey);
+        DescribeTimeToLiveResponse describeTimeToLiveResponse = getDynamoDbClient().describeTimeToLive(DescribeTimeToLiveRequest.builder()
+                .tableName(table.tableName()).build());
+        if (TimeToLiveStatus.ENABLED.equals(describeTimeToLiveResponse.timeToLiveDescription().timeToLiveStatus())) {
+            var ttlAttributeName = describeTimeToLiveResponse.timeToLiveDescription().attributeName();
+            return () -> ttlAttributeName;
+        }
+        return unsupportedTTL(table.tableName());
+    }
+
+    private Supplier<String> unsupportedTTL(String tableName) {
+        return () -> tableName + " don't support TTL operations. Check if TTL support is enabled for this table.";
+    }
+
+    private DynamoDbTable<EnhancedDocument> getOrCreateTable(String nameKey) {
+        DynamoDbTable<EnhancedDocument> table = dynamoDbEnhancedClient
+                .table(nameKey, TableSchema.documentSchemaBuilder()
+                        .addIndexPartitionKey(TableMetadata.primaryIndexName(), getEntityFieldName(), AttributeValueType.S)
+                        .addIndexSortKey(TableMetadata.primaryIndexName(), DocumentEntityConverter.ID, AttributeValueType.S)
+                        .attributeConverterProviders(AttributeConverterProvider.defaultProvider())
+                        .build());
+        try {
+            table.describeTable();
+            return table;
+        } catch (ResourceNotFoundException ex) {
+            if (shouldCreateTables()) {
+                table.createTable();
+                return table;
+            }
+            throw ex;
+        }
+    }
+
+    private boolean shouldCreateTables() {
+        return this.settings
+                .get(DynamoDBConfigurations.CREATE_TABLES, Boolean.class)
+                .orElse(false);
+    }
+
+    private String getEntityFieldName() {
+        return getEntityNameResolver().apply(DocumentEntityConverter.ENTITY);
     }
 
     @Override
-    public DocumentEntity insert(DocumentEntity documentEntity, Duration duration) {
-        return null;
+    public DocumentEntity insert(DocumentEntity documentEntity, Duration ttl) {
+        requireNonNull(documentEntity, "documentEntity is required");
+        requireNonNull(ttl, "ttl is required");
+        DynamoDbTable<EnhancedDocument> tableFor = getTableFor(documentEntity.name());
+        documentEntity.add(getTTLAttributeNameFor(tableFor.tableName()).get(), Instant.now().plus(ttl).truncatedTo(ChronoUnit.SECONDS));
+        var enhancedDocument = convertToEnhancedDocument(documentEntity);
+        tableFor.putItem(enhancedDocument);
+        return documentEntity;
     }
 
     @Override
-    public Iterable<DocumentEntity> insert(Iterable<DocumentEntity> iterable) {
-        return null;
+    public Iterable<DocumentEntity> insert(Iterable<DocumentEntity> entities) {
+        requireNonNull(entities, "entities is required");
+        return StreamSupport.stream(entities.spliterator(), false)
+                .map(this::insert)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Iterable<DocumentEntity> insert(Iterable<DocumentEntity> iterable, Duration duration) {
-        return null;
+    public Iterable<DocumentEntity> insert(Iterable<DocumentEntity> entities, Duration ttl) {
+        requireNonNull(entities, "entities is required");
+        requireNonNull(ttl, "ttl is required");
+        return StreamSupport.stream(entities.spliterator(), false)
+                .map(e -> this.insert(e, ttl))
+                .collect(Collectors.toList());
     }
 
     @Override
--- jnosql-dynamodb/src/test/java/org/eclipse/jnosql/databases/dynamodb/communication/DynamoDBDocumentManagerTest.java
+++ jnosql-dynamodb/src/test/java/org/eclipse/jnosql/databases/dynamodb/communication/DynamoDBDocumentManagerTest.java
@@ -17,28 +17,125 @@ package org.eclipse.jnosql.databases.dynamodb.communication;
 
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
+import org.eclipse.jnosql.communication.document.Document;
+import org.eclipse.jnosql.communication.document.DocumentEntity;
 import org.eclipse.jnosql.communication.document.DocumentManager;
+import org.eclipse.jnosql.mapping.core.config.MappingConfigurations;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import software.amazon.awssdk.enhanced.dynamodb.*;
+import software.amazon.awssdk.enhanced.dynamodb.document.EnhancedDocument;
+import software.amazon.awssdk.enhanced.dynamodb.model.DescribeTableEnhancedResponse;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
 
+import java.time.Duration;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.eclipse.jnosql.communication.driver.IntegrationTest.MATCHES;
 import static org.eclipse.jnosql.communication.driver.IntegrationTest.NAMED;
+import static org.eclipse.jnosql.databases.dynamodb.communication.DynamoDBTestUtils.CONFIG;
+import static org.eclipse.jnosql.databases.dynamodb.communication.DynamoDBTestUtils.createTable;
 
 @EnabledIfSystemProperty(named = NAMED, matches = MATCHES)
 class DynamoDBDocumentManagerTest {
 
     private DocumentManager documentManager;
 
+    private DynamoDbClient dynamoDbClient;
+
+    private DynamoDbEnhancedClient dynamoDbEnhancedClient;
+
+    private DynamoDbTable<EnhancedDocument> table;
+
+    private String database;
+    private UnaryOperator<String> entityNameResolver;
+    private boolean tableWasCreated;
+
     @BeforeEach
     void setUp() {
-        documentManager = DynamoDBTestUtils.INSTANCE.getDocumentManagerFactory().apply("default");
+        var settings = CONFIG.getSettings(CONFIG.getDynamoDBHost("localhost", 8000));
+        database = settings.get(MappingConfigurations.DOCUMENT_DATABASE, String.class).orElseThrow();
+        entityNameResolver = entityName -> settings.get(DynamoDBConfigurations.ENTITY_PARTITION_KEY, String.class).orElse(entityName);
+
+        var documentManagerFactory = CONFIG.getDocumentManagerFactory(settings);
+        documentManager = documentManagerFactory.apply(database);
         Assertions.assertThat(documentManager).isNotNull();
+
+        dynamoDbClient = CONFIG.getDynamoDbClient(settings);
+        dynamoDbEnhancedClient = CONFIG.getDynamoDbEnhancedClient(dynamoDbClient);
+        table = dynamoDbEnhancedClient.table("music",
+                TableSchema.documentSchemaBuilder()
+                        .addIndexPartitionKey(TableMetadata.primaryIndexName(), entityNameResolver.apply("@entity"), AttributeValueType.S)
+                        .addIndexSortKey(TableMetadata.primaryIndexName(), "_id", AttributeValueType.S)
+                        .attributeConverterProviders(AttributeConverterProvider.defaultProvider())
+                        .build());
+
+        try {
+            table.describeTable();
+        } catch (ResourceNotFoundException ex) {
+            table.createTable();
+            tableWasCreated = true;
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (tableWasCreated)
+            table.deleteTable();
     }
 
     @Test
-    void shouldReturnName(){
-        Assertions.assertThat(documentManager.name()).isEqualTo("default");
+    void shouldReturnName() {
+        Assertions.assertThat(documentManager.name()).isEqualTo(database);
     }
+
+    @Test
+    void shouldReturnErrorWhenInsertNull() {
+        assertSoftly(softly -> {
+            softly.assertThatThrownBy(() -> documentManager.insert((DocumentEntity) null))
+                    .as("should return error when insert a null DocumentEntity reference")
+                    .isExactlyInstanceOf(NullPointerException.class);
+            softly.assertThatThrownBy(() -> documentManager.insert((DocumentEntity) null, Duration.ofSeconds(1)))
+                    .as("should return error when insert a null DocumentEntity reference with TTL param")
+                    .isInstanceOfAny(NullPointerException.class);
+            softly.assertThatThrownBy(() -> documentManager.insert((DocumentEntity) null, null))
+                    .as("should return error when insert a null DocumentEntity reference with nullable TTL param")
+                    .isInstanceOfAny(NullPointerException.class);
+            softly.assertThatThrownBy(() -> documentManager.insert((Iterable<DocumentEntity>) null))
+                    .as("should return error when insert a null Iterable<DocumentEntity> reference")
+                    .isInstanceOfAny(NullPointerException.class);
+            softly.assertThatThrownBy(() -> documentManager.insert((Iterable<DocumentEntity>) null, Duration.ofSeconds(1)))
+                    .as("should return error when insert a null Iterable<DocumentEntity> reference with TTL param")
+                    .isInstanceOfAny(NullPointerException.class);
+            softly.assertThatThrownBy(() -> documentManager.insert((Iterable<DocumentEntity>) null, null))
+                    .as("should return error when insert a null Iterable<DocumentEntity> reference with nullable TTL param")
+                    .isInstanceOfAny(NullPointerException.class);
+        });
+    }
+
+    @Test
+    void shouldInsertDocumentWithNoSubDocuments() {
+
+        DocumentEntity entity = DocumentEntityGenerator.getEntityWithSubDocuments(0);
+        var _entityType = entity.name();
+        var id = entity.find("_id", String.class).orElseThrow();
+        var persistedEntity = documentManager.insert(entity);
+        assertSoftly(softly -> {
+            softly.assertThat(persistedEntity).as("should return the persistent document entity from documentManager.insert() method").isNotNull();
+            EnhancedDocument persistedItem = table.getItem(Key.builder().partitionValue(_entityType).sortValue(id).build());
+            softly.assertThat(persistedItem).as("should return the item from dynamodb").isNotNull();
+        });
+
+
+    }
+
 
 }
