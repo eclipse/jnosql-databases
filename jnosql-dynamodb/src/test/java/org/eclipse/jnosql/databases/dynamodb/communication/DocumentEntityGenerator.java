129f7db61760893d12259dc8bd40eabb57b8cb32
Maximillian Arruda <dearrudam@gmail.com>
2024-01-07 16:36:17 -0300

    test: rename methods and using DataFaker
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- jnosql-dynamodb/src/test/java/org/eclipse/jnosql/databases/dynamodb/communication/DocumentEntityGenerator.java
+++ jnosql-dynamodb/src/test/java/org/eclipse/jnosql/databases/dynamodb/communication/DocumentEntityGenerator.java
@@ -15,6 +15,7 @@
 
 package org.eclipse.jnosql.databases.dynamodb.communication;
 
+import net.datafaker.Faker;
 import org.eclipse.jnosql.communication.document.Document;
 import org.eclipse.jnosql.communication.document.DocumentEntity;
 import org.eclipse.jnosql.communication.document.Documents;
@@ -31,21 +32,23 @@ import java.util.function.Consumer;
 final class DocumentEntityGenerator {
 
     static final String COLLECTION_NAME = "music";
+    static final Faker faker = new Faker();
 
-    static DocumentEntity getEntity() {
-        return getEntityWithSubDocuments(0);
+    static DocumentEntity createRandomEntity() {
+        return createRandomEntityWithSubDocuments(0);
     }
 
-    static DocumentEntity getEntityWithSubDocuments(int levels) {
+    static DocumentEntity createRandomEntityWithSubDocuments(int levels) {
         Map<String, Object> map = new HashMap<>();
         map.put("_id", UUID.randomUUID().toString());
-        map.put("name", "Poliana");
+        map.put("name", faker.name().firstName());
         map.put("hoje", LocalDate.now());
         map.put("agora", LocalDateTime.now());
-        map.put("integer", 1);
-        map.put("float", 1f);
+        map.put("integerNumber", faker.random().nextInt(1, 10));
+        map.put("floatNumber", (float) faker.random().nextDouble(1.0, 10.0));
+        map.put("doubleNumber",  faker.random().nextDouble(1.0, 10.0));
         map.put("bigdecimal", BigDecimal.valueOf(10.10));
-        map.put("city", "Salvador");
+        map.put("city", faker.address().city());
         map.put("texts", List.of("A", "B", "C"));
         if (levels > 0) {
             addSubDocument(m -> map.put("level" + levels, m), levels - 1);
@@ -62,8 +65,9 @@ final class DocumentEntityGenerator {
         map.put("text", UUID.randomUUID().toString());
         map.put("hoje", LocalDate.now());
         map.put("agora", LocalDateTime.now());
-        map.put("integer", 1);
-        map.put("float", 1f);
+        map.put("integerNumber", faker.random().nextInt(1, 10));
+        map.put("floatNumber", (float) faker.random().nextDouble(1.0, 10.0));
+        map.put("doubleNumber",  faker.random().nextDouble(1.0, 10.0));
         map.put("bigdecimal", BigDecimal.valueOf(10.10));
         if (level > 0) {
             addSubDocument(m -> map.put("level" + level, m), level - 1);
