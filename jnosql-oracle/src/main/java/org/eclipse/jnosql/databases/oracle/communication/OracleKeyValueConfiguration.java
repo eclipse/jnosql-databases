4e984b6efe5caa9697f4c3aa039b8cb3f17d9b57
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-16 19:41:04 +0000

    feat: update oracle keyvalue configuration using the centralized configuration
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/OracleKeyValueConfiguration.java
+++ jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/OracleKeyValueConfiguration.java
@@ -14,15 +14,9 @@
  */
 package org.eclipse.jnosql.databases.oracle.communication;
 
-import oracle.nosql.driver.NoSQLHandleConfig;
-import oracle.nosql.driver.NoSQLHandleFactory;
-import oracle.nosql.driver.kv.StoreAccessTokenProvider;
-import org.eclipse.jnosql.communication.Configurations;
 import org.eclipse.jnosql.communication.Settings;
 import org.eclipse.jnosql.communication.keyvalue.BucketManagerFactory;
 import org.eclipse.jnosql.communication.keyvalue.KeyValueConfiguration;
-
-import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
@@ -35,20 +29,8 @@ public class OracleKeyValueConfiguration implements KeyValueConfiguration {
     @Override
     public BucketManagerFactory apply(Settings settings) {
         requireNonNull(settings, "settings is required");
-        String host = settings.get(List.of(OracleConfigurations.HOST.get(), Configurations.HOST.get()))
-                .map(Object::toString).orElse("http://localhost:8080");
-        String user = settings.get(List.of(OracleConfigurations.USER.get(), Configurations.USER.get()))
-                .map(Object::toString).orElse(null);
-        String password = settings.get(List.of(OracleConfigurations.PASSWORD.get(), Configurations.PASSWORD.get()))
-                .map(Object::toString).orElse(null);
 
-        NoSQLHandleConfig config = new NoSQLHandleConfig(host);
-        if (user != null && password != null) {
-            config.setAuthorizationProvider(new StoreAccessTokenProvider(user, password.toCharArray()));
-        } else {
-            config.setAuthorizationProvider(new StoreAccessTokenProvider());
-        }
-        var nosql =  NoSQLHandleFactory.createNoSQLHandle(config);
+        var nosql =  NoSQLHandleConfigConfiguration.INSTANCE.apply(settings);
         return new OracleBucketManagerFactory(nosql);
     }
 }
