f786ae67e92438178b7064f13de2b1a398fea882
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-16 19:14:04 +0000

    feat: include header at the badtabase file
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-oracle/src/test/java/org/eclipse/jnosql/databases/oracle/communication/Database.java
+++ jnosql-oracle/src/test/java/org/eclipse/jnosql/databases/oracle/communication/Database.java
@@ -1,10 +1,23 @@
+/*
+ *  Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
 package org.eclipse.jnosql.databases.oracle.communication;
 
 import oracle.nosql.driver.NoSQLHandle;
 import oracle.nosql.driver.NoSQLHandleConfig;
 import oracle.nosql.driver.NoSQLHandleFactory;
 import oracle.nosql.driver.kv.StoreAccessTokenProvider;
-import org.eclipse.jnosql.communication.Configurations;
 import org.eclipse.jnosql.communication.Settings;
 import org.eclipse.jnosql.communication.keyvalue.BucketManagerFactory;
 import org.eclipse.jnosql.communication.keyvalue.KeyValueConfiguration;
