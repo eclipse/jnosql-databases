4f9d84d456313aee5c41668fe5df9c4f7fe5e12f
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-16 19:40:42 +0000

    feat: centralize the configuration at a single class
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/NoSQLHandleConfigConfiguration.java
+++ jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/NoSQLHandleConfigConfiguration.java
@@ -10,14 +10,20 @@ import org.eclipse.jnosql.communication.Settings;
 import java.util.List;
 import java.util.function.Function;
 
-enum NoSQLHandleConfigFactory implements Function<Settings, NoSQLHandle> {
+enum NoSQLHandleConfigConfiguration implements Function<Settings, NoSQLHandle> {
 
     INSTANCE;
 
+    private static final String DEFAULT_HOST = "http://localhost:8080";
+    private static final String DEFAULT_TABLE_READ_LIMITS = "http://localhost:8080";
+    private static final String DEFAULT_TABLE_WRITE_LIMITS = "http://localhost:8080";
+    private static final String DEFAULT_TABLE_STORAGE_GB = "http://localhost:8080";
+    private static final String DEFAULT_TABLE_WAIT_MILLIS = "http://localhost:8080";
+    private static final String DEFAULT_TABLE_DELAY_MILLIS = "http://localhost:8080";
     @Override
     public NoSQLHandle apply(Settings settings) {
         String host = settings.get(List.of(OracleConfigurations.HOST.get(), Configurations.HOST.get()))
-                .map(Object::toString).orElse("http://localhost:8080");
+                .map(Object::toString).orElse(DEFAULT_HOST);
         String user = settings.get(List.of(OracleConfigurations.USER.get(), Configurations.USER.get()))
                 .map(Object::toString).orElse(null);
         String password = settings.get(List.of(OracleConfigurations.PASSWORD.get(), Configurations.PASSWORD.get()))
