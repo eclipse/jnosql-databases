5b61f9855ade6975a1218e677d2c2ab991d23ed1
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-17 04:20:26 +0000

    feat: create structure to insert and update
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/OracleDocumentManager.java
+++ jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/OracleDocumentManager.java
@@ -16,16 +16,25 @@ package org.eclipse.jnosql.databases.oracle.communication;
 
 import jakarta.json.bind.Jsonb;
 import oracle.nosql.driver.NoSQLHandle;
+import oracle.nosql.driver.ops.PutRequest;
+import oracle.nosql.driver.values.JsonOptions;
+import oracle.nosql.driver.values.MapValue;
+import org.eclipse.jnosql.communication.document.Document;
 import org.eclipse.jnosql.communication.document.DocumentDeleteQuery;
 import org.eclipse.jnosql.communication.document.DocumentEntity;
 import org.eclipse.jnosql.communication.document.DocumentManager;
 import org.eclipse.jnosql.communication.document.DocumentQuery;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 final class OracleDocumentManager implements DocumentManager {
+    static final String ENTITY = "_entity";
+    static final String ID = "_id";
     private final String table;
     private final NoSQLHandle serviceHandle;
 
@@ -44,33 +53,48 @@ final class OracleDocumentManager implements DocumentManager {
     @Override
     public DocumentEntity insert(DocumentEntity entity) {
         Objects.requireNonNull(entity, "entity is required");
+        Map<String, Object> entityMap = new HashMap<>(entity.toMap());
+        entityMap.put(ENTITY, entity.name());
+        String id = entity.find(ID).map(Document::get)
+                .map(Object::toString)
+                .orElseThrow(() -> new OracleDBException("The _id is required in the entity"));
 
-        return null;
+        MapValue mapValue = new MapValue().put("id", id);
+        MapValue contentVal = mapValue.putFromJson("content", jsonB.toJson(entityMap),
+                new JsonOptions());
+        PutRequest putRequest = new PutRequest()
+                .setValue(contentVal)
+                .setTableName(name());
+
+        serviceHandle.put(putRequest);
+        return entity;
     }
 
     @Override
     public DocumentEntity insert(DocumentEntity entity, Duration ttl) {
-        return null;
+       throw new UnsupportedOperationException("Oracle NoSQL database does not support TTL");
     }
 
     @Override
     public Iterable<DocumentEntity> insert(Iterable<DocumentEntity> entities) {
-        return null;
+        Objects.requireNonNull(entities, "entities is required");
+        StreamSupport.stream(entities.spliterator(), false).forEach(this::insert);
+        return entities;
     }
 
     @Override
     public Iterable<DocumentEntity> insert(Iterable<DocumentEntity> entities, Duration ttl) {
-        return null;
+        throw new UnsupportedOperationException("Oracle NoSQL database does not support TTL");
     }
 
     @Override
     public DocumentEntity update(DocumentEntity entity) {
-        return null;
+        return insert(entity);
     }
 
     @Override
     public Iterable<DocumentEntity> update(Iterable<DocumentEntity> entities) {
-        return null;
+        return insert(entities);
     }
 
     @Override
