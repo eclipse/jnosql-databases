4b4c59ce11ea9aa22e2d9bc40e6efc859b52bd01
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-22 17:50:11 +0000

    docs: create documentation to Oracle template
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/mapping/OracleTemplate.java
+++ jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/mapping/OracleTemplate.java
@@ -14,7 +14,45 @@
  */
 package org.eclipse.jnosql.databases.oracle.mapping;
 
+import org.eclipse.jnosql.communication.document.DocumentEntity;
 import org.eclipse.jnosql.mapping.document.JNoSQLDocumentTemplate;
 
+import java.util.stream.Stream;
+
+/**
+ * The {@code OracleTemplate} is an interface that extends {@link JNoSQLDocumentTemplate} and
+ * provides methods for executing Oracle SQL queries using the Oracle Query Language (SQL).
+ * <p>
+ * It allows you to interact with the Oracle NoSQL database using SQL queries to retrieve and
+ * process data in a more flexible and customizable way.
+ * </p>
+ *
+ * @see JNoSQLDocumentTemplate
+ */
 public interface OracleTemplate extends JNoSQLDocumentTemplate {
+
+    /**
+     * Executes an Oracle query using the Oracle Query Language (SQL).
+     *
+     * @param query the SQL query
+     * @return a {@link Stream} of results representing the query result
+     * @throws NullPointerException when the query is null
+     *
+     * @param <T> the type of objects in the result stream
+     */
+    <T> Stream<T> sql(String query);
+
+    /**
+     * Executes an Oracle query using the Oracle Query Language (SQL) with parameters.
+     * <p>Example query: {@code SELECT * FROM users WHERE status = ?}</p>
+     *
+     * @param query  the SQL query with placeholders for parameters
+     * @param params the values to replace the placeholders in the query
+     * @return a {@link Stream} of results representing the query result
+     * @throws NullPointerException when either the query or params are null
+     *
+     * @param <T> the type of objects in the result stream
+     */
+    <T>  Stream<T> sql(String query, Object... params);
+
 }
