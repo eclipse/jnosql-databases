52ddc9d27865e94fcc680d0d1a58f7aae94cc9ec
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-22 17:41:16 +0000

    feat: create sql annotation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/mapping/SQL.java
+++ jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/mapping/SQL.java
@@ -1,2 +1,18 @@
-package org.eclipse.jnosql.databases.oracle.mapping;public @interface SQL {
+/*
+ *  Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.oracle.mapping;
+
+public @interface SQL {
 }
