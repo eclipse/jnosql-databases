5f9f4b9d1886756918af88b7deafbd8b565fe686
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-23 07:28:52 +0000

    feat: create deployment type
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/DeploymentType.java
+++ jnosql-oracle/src/main/java/org/eclipse/jnosql/databases/oracle/communication/DeploymentType.java
@@ -14,22 +14,71 @@
  */
 package org.eclipse.jnosql.databases.oracle.communication;
 
+import oracle.nosql.driver.AuthorizationProvider;
+import oracle.nosql.driver.iam.SignatureProvider;
+import oracle.nosql.driver.kv.StoreAccessTokenProvider;
+import org.eclipse.jnosql.communication.Configurations;
+import org.eclipse.jnosql.communication.Settings;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
 /**
  * The {@code DeploymentType} enum represents different deployment options for software solutions.
  * It is used to categorize deployments as either "On-premises" or "Cloud."
  */
-public enum DeploymentType {
+public enum DeploymentType implements Function<Settings, Optional<AuthorizationProvider>> {
     /**
      * Represents an "On-premises" deployment where software solutions are deployed and managed
      * within an organization's physical premises or data centers.
      */
-    ON_PREMISES,
+    ON_PREMISES{
+        @Override
+        public Optional<AuthorizationProvider> apply(Settings settings) {
+            String user = settings.get(List.of(OracleNoSQLConfigurations.USER.get(), Configurations.USER.get()))
+                    .map(Object::toString).orElse(null);
+            String password = settings.get(List.of(OracleNoSQLConfigurations.PASSWORD.get(), Configurations.PASSWORD.get()))
+                    .map(Object::toString).orElse(null);
+            if (user != null && password != null) {
+               return Optional.of(new StoreAccessTokenProvider(user, password.toCharArray()));
+            } else {
+                return Optional.empty();
+            }
+
+        }
+    },
 
     /**
      * Represents a "Cloud" deployment where software solutions are delivered and managed
-     * over the internet (the cloud) by third-party cloud providers.
+     * over the internet (the cloud) by Oracle cloud provider.
      */
-    CLOUD;
+    CLOUD{
+        @Override
+        public Optional<AuthorizationProvider> apply(Settings settings) {
+
+            String user = settings.get(List.of(OracleNoSQLConfigurations.USER.get(), Configurations.USER.get()))
+                    .map(Object::toString).orElse(null);
+            char[] password = settings.get(List.of(OracleNoSQLConfigurations.PASSWORD.get(), Configurations.PASSWORD.get()))
+                    .map(Object::toString).map(String::toCharArray).orElse(new char[0]);
+            String tenantId = settings.get(OracleNoSQLConfigurations.TENANT, String.class).orElse(null);
+            String fingerprint= settings.get(OracleNoSQLConfigurations.FINGERPRINT, String.class).orElse(null);
+            String privateKey = settings.get(OracleNoSQLConfigurations.PRIVATE_KEY, String.class).orElse(null);
+
+            if(user != null && password.length > 0 && tenantId != null && fingerprint != null && privateKey != null) {
+                return Optional.of(new SignatureProvider(tenantId, user, fingerprint, privateKey, password));
+            }
+
+            try {
+                return Optional.of(new SignatureProvider());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    };
+
 
     /**
      * Parses a string representation of a deployment type into the {@link DeploymentType} enum,
