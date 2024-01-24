072fe632bca87b1f2f35415e0e25f9501bdf25d4
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-24 05:21:41 +0000

    feat: remove dead code
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-nosql-oracle/src/test/java/org/eclipse/jnosql/databases/oracle/communication/Database.java
+++ jnosql-nosql-oracle/src/test/java/org/eclipse/jnosql/databases/oracle/communication/Database.java
@@ -14,10 +14,6 @@
  */
 package org.eclipse.jnosql.databases.oracle.communication;
 
-import oracle.nosql.driver.NoSQLHandle;
-import oracle.nosql.driver.NoSQLHandleConfig;
-import oracle.nosql.driver.NoSQLHandleFactory;
-import oracle.nosql.driver.kv.StoreAccessTokenProvider;
 import org.eclipse.jnosql.communication.Settings;
 import org.eclipse.jnosql.communication.document.DocumentConfiguration;
 import org.eclipse.jnosql.communication.document.DocumentManagerFactory;
@@ -39,15 +35,6 @@ public enum Database implements Supplier<BucketManagerFactory> {
         container.start();
     }
 
-    NoSQLHandle getNoSQLHandle() {
-        String address = container.getHost();
-        Integer port = container.getFirstMappedPort();
-        //NoSQLHandleConfig config = new NoSQLHandleConfig("http://" + System.getenv("NOSQL_ENDPOINT") + ":" + System.getenv("NOSQL_PORT"));
-        System.out.println("Connecting to http://" + address + ":" + port);
-        NoSQLHandleConfig config = new NoSQLHandleConfig("http://" + address + ":" + port);
-        config.setAuthorizationProvider(new StoreAccessTokenProvider());
-        return NoSQLHandleFactory.createNoSQLHandle(config) ;
-    }
 
     @Override
     public BucketManagerFactory get() {
