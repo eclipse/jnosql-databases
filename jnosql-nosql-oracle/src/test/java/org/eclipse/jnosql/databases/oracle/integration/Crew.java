1bc780ff7beb6107df50f31ccca0e1019ff103fb
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-30 11:06:03 +0000

    feat: create insert scenario
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-nosql-oracle/src/test/java/org/eclipse/jnosql/databases/oracle/integration/Crew.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.oracle.integration;
+
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+
+import java.util.Objects;
+
+@Entity
+public class Crew {
+
+    @Column
+    private String name;
+
+    public String name() {
+        return name;
+    }
+
+
+    Crew() {
+    }
+
+    Crew(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Crew crew = (Crew) o;
+        return Objects.equals(name, crew.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Crew{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
