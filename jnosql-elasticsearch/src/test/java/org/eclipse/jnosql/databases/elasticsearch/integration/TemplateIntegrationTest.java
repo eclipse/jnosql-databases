0d3ab0a0a01eec982e8b2e539d377d8d7a26bb55
Maximillian Arruda <dearrudam@gmail.com>
2024-03-12 15:57:11 -0300

    test(integration): fixed @AddPackage declaration
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- jnosql-elasticsearch/src/test/java/org/eclipse/jnosql/databases/elasticsearch/integration/TemplateIntegrationTest.java
+++ jnosql-elasticsearch/src/test/java/org/eclipse/jnosql/databases/elasticsearch/integration/TemplateIntegrationTest.java
@@ -19,13 +19,13 @@ import jakarta.inject.Inject;
 import org.awaitility.Awaitility;
 import org.eclipse.jnosql.databases.elasticsearch.communication.DocumentDatabase;
 import org.eclipse.jnosql.databases.elasticsearch.communication.ElasticsearchConfigurations;
-import jakarta.nosql.Convert;
 import org.eclipse.jnosql.mapping.Database;
+import org.eclipse.jnosql.mapping.core.Converters;
 import org.eclipse.jnosql.mapping.core.config.MappingConfigurations;
+import org.eclipse.jnosql.mapping.core.spi.EntityMetadataExtension;
 import org.eclipse.jnosql.mapping.document.DocumentTemplate;
 import org.eclipse.jnosql.mapping.document.spi.DocumentExtension;
 import org.eclipse.jnosql.mapping.reflection.Reflections;
-import org.eclipse.jnosql.mapping.core.spi.EntityMetadataExtension;
 import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
@@ -47,7 +47,7 @@ import static org.eclipse.jnosql.communication.driver.IntegrationTest.MATCHES;
 import static org.eclipse.jnosql.communication.driver.IntegrationTest.NAMED;
 
 @EnableAutoWeld
-@AddPackages(value = {Database.class, EntityConverter.class, DocumentTemplate.class})
+@AddPackages(value = {Database.class, Converters.class, EntityConverter.class, DocumentTemplate.class})
 @AddPackages(Book.class)
 @AddPackages(Reflections.class)
 @AddExtensions({EntityMetadataExtension.class,
