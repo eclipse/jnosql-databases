28583c5c9d0824fa45c69de5b1d5e65a35ae5281
Otavio Santana <otaviopolianasantana@gmail.com>
2024-09-02 17:27:10 +0100

    test: create limit and skip using mapping at arangodb
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-arangodb/src/test/java/org/eclipse/jnosql/databases/arangodb/integration/ArangoDBTemplateIntegrationTest.java
+++ jnosql-arangodb/src/test/java/org/eclipse/jnosql/databases/arangodb/integration/ArangoDBTemplateIntegrationTest.java
@@ -30,9 +30,11 @@ import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
+import java.util.List;
 import java.util.Optional;
 
 import static java.util.UUID.randomUUID;
@@ -59,6 +61,11 @@ class ArangoDBTemplateIntegrationTest {
         INSTANCE.get("library");
         System.setProperty(ArangoDBConfigurations.HOST.get() + ".1", INSTANCE.host());
         System.setProperty(MappingConfigurations.DOCUMENT_DATABASE.get(), "library");
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.template.delete(Book.class).execute();
     }
 
     @Test
@@ -112,7 +119,7 @@ class ArangoDBTemplateIntegrationTest {
     }
 
     @Test
-    void shouldDeleteAll(){
+    void shouldDeleteAll() {
         for (int index = 0; index < 20; index++) {
             Book book = new Book(randomUUID().toString(), "Effective Java", 1);
             assertThat(template.insert(book))
@@ -126,7 +133,7 @@ class ArangoDBTemplateIntegrationTest {
 
 
     @Test
-    void shouldUpdateNullValues(){
+    void shouldUpdateNullValues() {
         var book = new Book(randomUUID().toString(), "Effective Java", 1);
         template.insert(book);
         template.update(new Book(book.id(), null, 2));
@@ -138,7 +145,57 @@ class ArangoDBTemplateIntegrationTest {
             softly.assertThat(optional).get().extracting(Book::edition).isEqualTo(2);
         });
     }
-    
 
+    @Test
+    void shouldExecuteLimit() {
 
+        for (int index = 1; index < 10; index++) {
+            var book = new Book(randomUUID().toString(), "Effective Java", index);
+            template.insert(book);
+        }
+
+        List<Book> books = template.select(Book.class).orderBy("edition")
+                .asc().limit(4).result();
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(books).hasSize(4);
+            var editions = books.stream().map(Book::edition).toList();
+            soft.assertThat(editions).hasSize(4).contains(1, 2, 3, 4);
+        });
+
+    }
+
+    @Test
+    void shouldExecuteSkip() {
+        for (int index = 1; index < 10; index++) {
+            var book = new Book(randomUUID().toString(), "Effective Java", index);
+            template.insert(book);
+        }
+
+        List<Book> books = template.select(Book.class).orderBy("edition")
+                .asc().skip(4).result();
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(books).hasSize(5);
+            var editions = books.stream().map(Book::edition).toList();
+            soft.assertThat(editions).hasSize(5).contains(5, 6, 7, 8, 9);
+        });
+    }
+
+    @Test
+    void shouldExecuteLimitStart() {
+        for (int index = 1; index < 10; index++) {
+            var book = new Book(randomUUID().toString(), "Effective Java", index);
+            template.insert(book);
+        }
+
+        List<Book> books = template.select(Book.class).orderBy("edition")
+                .asc().skip(4).limit(3).result();
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(books).hasSize(3);
+            var editions = books.stream().map(Book::edition).toList();
+            soft.assertThat(editions).hasSize(3).contains(5, 6, 7);
+        });
+    }
 }
