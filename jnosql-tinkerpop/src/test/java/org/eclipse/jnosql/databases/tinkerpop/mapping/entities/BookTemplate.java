87d4f90ec7600568e43fb7b01bd2e47c8bdce8d6
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-10 19:21:48 +0000

    feat: update apache tinkerpop api
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-tinkerpop/src/test/java/org/eclipse/jnosql/databases/tinkerpop/mapping/entities/BookTemplate.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.tinkerpop.mapping.entities;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.jnosql.databases.tinkerpop.mapping.GraphTemplate;
+import org.eclipse.jnosql.databases.tinkerpop.mapping.Transactional;
+
+@ApplicationScoped
+public class BookTemplate {
+
+    @Inject
+    private GraphTemplate graphTemplate;
+
+    @Transactional
+    public void insert(Book actor) {
+        graphTemplate.insert(actor);
+    }
+
+    @Transactional
+    public void insertException(Book actor) {
+        graphTemplate.insert(actor);
+        throw new NullPointerException("should get a rollback");
+    }
+
+    public void normalInsertion(Book actor) {
+        graphTemplate.insert(actor);
+    }
+
+}
